5f694224cff9ac6a46770b938c02d80fe2718dfd
BadBones69 <joewojcik14@gmail.com>
2019-04-21 17:58:20 -0400

    Added Missing 1.13 enchantments
    Added:
    - Riptide
    - Channeling
    - Impaling
    - Loyalty

--- src/main/java/me/badbones69/crazycrates/Methods.java
+++ src/main/java/me/badbones69/crazycrates/Methods.java
@@ -291,76 +291,54 @@ public class Methods {
 	}
 	
 	public static Set<String> getEnchantments() {
-		HashMap<String, String> enchants = new HashMap<>();
-		enchants.put("ARROW_DAMAGE", "Power");
-		enchants.put("ARROW_FIRE", "Flame");
-		enchants.put("ARROW_INFINITE", "Infinity");
-		enchants.put("ARROW_KNOCKBACK", "Punch");
-		enchants.put("DAMAGE_ALL", "Sharpness");
-		enchants.put("DAMAGE_ARTHROPODS", "Bane_Of_Arthropods");
-		enchants.put("DAMAGE_UNDEAD", "Smite");
-		enchants.put("DEPTH_STRIDER", "Depth_Strider");
-		enchants.put("DIG_SPEED", "Efficiency");
-		enchants.put("DURABILITY", "Unbreaking");
-		enchants.put("FIRE_ASPECT", "Fire_Aspect");
-		enchants.put("KNOCKBACK", "KnockBack");
-		enchants.put("LOOT_BONUS_BLOCKS", "Fortune");
-		enchants.put("LOOT_BONUS_MOBS", "Looting");
-		enchants.put("LUCK", "Luck_Of_The_Sea");
-		enchants.put("LURE", "Lure");
-		enchants.put("OXYGEN", "Respiration");
-		enchants.put("PROTECTION_ENVIRONMENTAL", "Protection");
-		enchants.put("PROTECTION_EXPLOSIONS", "Blast_Protection");
-		enchants.put("PROTECTION_FALL", "Feather_Falling");
-		enchants.put("PROTECTION_FIRE", "Fire_Protection");
-		enchants.put("PROTECTION_PROJECTILE", "Projectile_Protection");
-		enchants.put("SILK_TOUCH", "Silk_Touch");
-		enchants.put("THORNS", "Thorns");
-		enchants.put("WATER_WORKER", "Aqua_Affinity");
-		enchants.put("BINDING_CURSE", "Curse_Of_Binding");
-		enchants.put("MENDING", "Mending");
-		enchants.put("FROST_WALKER", "Frost_Walker");
-		enchants.put("VANISHING_CURSE", "Curse_Of_Vanishing");
-		enchants.put("SWEEPING_EDGE", "Sweeping_Edge");
-		return enchants.keySet();
+		return getEnchantmentList().keySet();
 	}
 	
 	public static String getEnchantmentName(Enchantment en) {
-		HashMap<String, String> enchants = new HashMap<>();
-		enchants.put("ARROW_DAMAGE", "Power");
-		enchants.put("ARROW_FIRE", "Flame");
-		enchants.put("ARROW_INFINITE", "Infinity");
-		enchants.put("ARROW_KNOCKBACK", "Punch");
-		enchants.put("DAMAGE_ALL", "Sharpness");
-		enchants.put("DAMAGE_ARTHROPODS", "Bane_Of_Arthropods");
-		enchants.put("DAMAGE_UNDEAD", "Smite");
-		enchants.put("DEPTH_STRIDER", "Depth_Strider");
-		enchants.put("DIG_SPEED", "Efficiency");
-		enchants.put("DURABILITY", "Unbreaking");
-		enchants.put("FIRE_ASPECT", "Fire_Aspect");
-		enchants.put("KNOCKBACK", "KnockBack");
-		enchants.put("LOOT_BONUS_BLOCKS", "Fortune");
-		enchants.put("LOOT_BONUS_MOBS", "Looting");
-		enchants.put("LUCK", "Luck_Of_The_Sea");
-		enchants.put("LURE", "Lure");
-		enchants.put("OXYGEN", "Respiration");
-		enchants.put("PROTECTION_ENVIRONMENTAL", "Protection");
-		enchants.put("PROTECTION_EXPLOSIONS", "Blast_Protection");
-		enchants.put("PROTECTION_FALL", "Feather_Falling");
-		enchants.put("PROTECTION_FIRE", "Fire_Protection");
-		enchants.put("PROTECTION_PROJECTILE", "Projectile_Protection");
-		enchants.put("SILK_TOUCH", "Silk_Touch");
-		enchants.put("THORNS", "Thorns");
-		enchants.put("WATER_WORKER", "Aqua_Affinity");
-		enchants.put("BINDING_CURSE", "Curse_Of_Binding");
-		enchants.put("MENDING", "Mending");
-		enchants.put("FROST_WALKER", "Frost_Walker");
-		enchants.put("VANISHING_CURSE", "Curse_Of_Vanishing");
-		enchants.put("SWEEPING_EDGE", "Sweeping_Edge");
+		HashMap<String, String> enchants = getEnchantmentList();
 		if(enchants.get(en.getName()) == null) {
 			return "None Found";
 		}
 		return enchants.get(en.getName());
+	}
+	
+	private static HashMap<String, String> getEnchantmentList() {
+		HashMap<String, String> enchants = new HashMap<>();
+		enchants.put("ARROW_DAMAGE", "Power");
+		enchants.put("ARROW_FIRE", "Flame");
+		enchants.put("ARROW_INFINITE", "Infinity");
+		enchants.put("ARROW_KNOCKBACK", "Punch");
+		enchants.put("DAMAGE_ALL", "Sharpness");
+		enchants.put("DAMAGE_ARTHROPODS", "Bane_Of_Arthropods");
+		enchants.put("DAMAGE_UNDEAD", "Smite");
+		enchants.put("DEPTH_STRIDER", "Depth_Strider");
+		enchants.put("DIG_SPEED", "Efficiency");
+		enchants.put("DURABILITY", "Unbreaking");
+		enchants.put("FIRE_ASPECT", "Fire_Aspect");
+		enchants.put("KNOCKBACK", "KnockBack");
+		enchants.put("LOOT_BONUS_BLOCKS", "Fortune");
+		enchants.put("LOOT_BONUS_MOBS", "Looting");
+		enchants.put("LUCK", "Luck_Of_The_Sea");
+		enchants.put("LURE", "Lure");
+		enchants.put("OXYGEN", "Respiration");
+		enchants.put("PROTECTION_ENVIRONMENTAL", "Protection");
+		enchants.put("PROTECTION_EXPLOSIONS", "Blast_Protection");
+		enchants.put("PROTECTION_FALL", "Feather_Falling");
+		enchants.put("PROTECTION_FIRE", "Fire_Protection");
+		enchants.put("PROTECTION_PROJECTILE", "Projectile_Protection");
+		enchants.put("SILK_TOUCH", "Silk_Touch");
+		enchants.put("THORNS", "Thorns");
+		enchants.put("WATER_WORKER", "Aqua_Affinity");
+		enchants.put("BINDING_CURSE", "Curse_Of_Binding");
+		enchants.put("MENDING", "Mending");
+		enchants.put("FROST_WALKER", "Frost_Walker");
+		enchants.put("VANISHING_CURSE", "Curse_Of_Vanishing");
+		enchants.put("SWEEPING_EDGE", "Sweeping_Edge");
+		enchants.put("RIPTIDE", "Riptide");
+		enchants.put("CHANNELING", "Channeling");
+		enchants.put("IMPALING", "Impaling");
+		enchants.put("LOYALTY", "Loyalty");
+		return enchants;
 	}
 	
 	public static Material getRandomPaneColor() {
