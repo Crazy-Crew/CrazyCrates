357c46b0c73070b98cdfebbdd56cc04f136d339c
BadBones69 <joewojcik14@gmail.com>
2021-03-27 01:28:54 -0400

    Code cleanup

--- plugin/src/main/java/me/badbones69/crazycrates/multisupport/Version.java
+++ plugin/src/main/java/me/badbones69/crazycrates/multisupport/Version.java
@@ -14,9 +14,7 @@ public enum Version {
     v1_13_R2(1132),
     v1_14_R1(1141),
     v1_15_R1(1151),
-    v1_16_R1(1161),
-    v1_16_R2(1162),
-    v1_16_R3(1163),
+    v1_16_R1(1161), v1_16_R2(1162), v1_16_R3(1163),
     TOO_NEW(-2);
     
     private static Version currentVersion;
