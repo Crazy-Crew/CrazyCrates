8bc57d63493017542959f7600b9eeb818d89f72f
QuakyCZ <info@quaky.cz>
2021-10-02 00:50:39 +0200

    Javadoc

--- plugin/src/main/java/me/badbones69/crazycrates/multisupport/OraxenSupport.java
+++ plugin/src/main/java/me/badbones69/crazycrates/multisupport/OraxenSupport.java
@@ -5,13 +5,24 @@ import org.bukkit.inventory.ItemStack;
 
 public class OraxenSupport {
     private static boolean loaded = false;
-    
+
+    /**
+     * Loads the support
+     */
     public static void load() {
         loaded = true;
     }
-    
+
+    /**
+     * @return true if OraxenSupport is loaded
+     */
     public static boolean isLoaded() { return loaded; }
-    
+
+    /**
+     * Get an ItemStack from the Oraxen plugin.
+     * @param name the name of the item in Oraxen config
+     * @return ItemStack if Oraxen has the item or null.
+     */
     public static ItemStack getItem(String name) {
         if(!loaded)
             return null;
