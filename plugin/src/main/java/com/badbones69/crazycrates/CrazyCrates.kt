6d75c074431e61aa517cad6d641444576513764b
Ryder Belserion <RyderBelserion@protonmail.com>
2022-01-25 12:03:19 -0500

    Added file conversion back

--- plugin/src/main/java/com/badbones69/crazycrates/CrazyCrates.kt
+++ plugin/src/main/java/com/badbones69/crazycrates/CrazyCrates.kt
@@ -1,6 +1,5 @@
 package com.badbones69.crazycrates;
 
-import io.papermc.lib.PaperLib
 import com.badbones69.crazycrates.api.CrazyManager
 import com.badbones69.crazycrates.api.FileManager
 import com.badbones69.crazycrates.api.FileManager.Files
@@ -18,9 +17,14 @@ import com.badbones69.crazycrates.support.libs.Support
 import com.badbones69.crazycrates.support.libs.Version
 import com.badbones69.crazycrates.support.placeholders.MVdWPlaceholderAPISupport
 import com.badbones69.crazycrates.support.placeholders.PlaceholderAPISupport
+import io.papermc.lib.PaperLib
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import java.io.IOException
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
 
 class CrazyCrates : JavaPlugin() {
 
@@ -30,8 +34,8 @@ class CrazyCrates : JavaPlugin() {
         if (Version.isOlder(Version.TOO_OLD)) {
             logger.warning("============= Crazy Crates =============");
             logger.info(" ")
-            logger.warning("You are running Crazy Crates on a version that is not 1.18.X, QuadCrates will not work")
-            logger.warning("Crazy Crates will function back to 1.13 and has no guarantee it will function any further.")
+            logger.warning("You are running Crazy Crates on a version that is not 1.18.X.")
+            logger.warning("No guarantee that it will run perfectly, You have been warned.")
             logger.info(" ")
             logger.warning("Jenkins Page: https://jenkins.badbones69.com/job/Crazy-Crates-Dev/")
             logger.warning("Version Integer: " + Version.getCurrentVersion().versionInteger)
@@ -42,6 +46,14 @@ class CrazyCrates : JavaPlugin() {
 
     override fun onEnable() {
         super.onEnable()
+
+        updateFiles("Locations.yml", "locations.yml")
+        updateFiles("Data.yml", "data.yml")
+        updateFiles("Config.yml", "config.yml")
+        updateFiles("Messages.yml", "messages.yml")
+
+        updateDirectory("Crates", "crates")
+        updateDirectory("Schematics", "schematics")
 
         if (PaperLib.isSpigot()) PaperLib.suggestPaper(this)
 
@@ -124,5 +136,35 @@ class CrazyCrates : JavaPlugin() {
     fun onPlayerJoin(e: PlayerJoinEvent): Unit = with(e) {
         CrazyManager.getInstance().setNewPlayerKeys(player)
         CrazyManager.getInstance().loadOfflinePlayersKeys(player)
+    }
+
+    private fun updateFiles(oldFileName: String, newFileName: String) {
+        val oldPath = Path.of("$dataFolder/$oldFileName")
+        if (java.nio.file.Files.exists(oldPath)) {
+            logger.warning("$oldFileName has been found.")
+            logger.warning("Converting to $newFileName")
+            val newFile = File(dataFolder, newFileName)
+            try {
+                java.nio.file.Files.copy(oldPath, newFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+                java.nio.file.Files.deleteIfExists(oldPath)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun updateDirectory(oldDirectory: String, newDirectory: String) {
+        val oldPath = Path.of("$dataFolder/$oldDirectory")
+        if (java.nio.file.Files.exists(oldPath)) {
+            logger.warning("$oldDirectory directory has been found.")
+            logger.warning("Converting $oldDirectory directory to $newDirectory")
+            val newDir = File(dataFolder, newDirectory)
+            try {
+                java.nio.file.Files.move(oldPath, newDir.toPath(), StandardCopyOption.REPLACE_EXISTING)
+                java.nio.file.Files.deleteIfExists(oldPath)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
