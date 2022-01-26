ee7fc037fe8ca0041b64bc296c139186f8c4f43e
Ryder Belserion <RyderBelserion@protonmail.com>
2022-01-26 17:46:02 -0500

    Fixes #393 - <#458588> is the format for now.

--- plugin/src/main/java/com/badbones69/crazycrates/func/Constants.kt
+++ plugin/src/main/java/com/badbones69/crazycrates/func/Constants.kt
@@ -5,17 +5,21 @@ import net.md_5.bungee.api.ChatColor
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 
-fun color(message: String): String {
-    val pattern = Pattern.compile("#[a-fA-F0-9]{6}")
-    val matcher: Matcher = pattern.matcher(message)
-    val buffer = StringBuffer()
+private val hexPattern = Pattern.compile("<#([A-Fa-f0-9]){6}>")
+
+fun color(message: String): String? {
+    var message = message
+    var matcher = hexPattern.matcher(message)
     while (matcher.find()) {
-        matcher.appendReplacement(buffer, ChatColor.of(matcher.group()).toString())
+        val hexColor = ChatColor.of(matcher.group().substring(1, matcher.group().length - 1))
+        val before = message.substring(0, matcher.start())
+        val after = message.substring(matcher.end())
+        message = before + hexColor + after
+        matcher = hexPattern.matcher(message)
     }
-    return org.bukkit.ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString())
+    return ChatColor.translateAlternateColorCodes('&', message)
 }
 
 fun getFile(plugin: JavaPlugin, name: String): File {
