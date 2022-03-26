175e8f16a8634872ec5dde65cb8f68ac074e180e
Ryder Belserion <RyderBelserion@protonmail.com>
2022-03-25 20:56:15 -0400

    Create a session manager for QuadCrates

---
+++ plugin/src/main/java/com/badbones69/crazycrates/v2/utils/quadcrates/sessions/SessionManager.kt
@@ -0,0 +1,27 @@
+package com.badbones69.crazycrates.v2.utils.quadcrates.sessions
+
+import com.badbones69.crazycrates.v2.crates.sessions.QuadCrateSession
+import org.bukkit.entity.Player
+import java.util.function.Consumer
+
+object SessionManager {
+
+    fun inSession(player: Player): Boolean {
+        QuadCrateSession.getCrateSessions().forEach {
+            if (it.player == player) return true
+        }
+        return false;
+    }
+
+    fun getSession(player: Player): QuadCrateSession? {
+        QuadCrateSession.getCrateSessions().forEach {
+            if (it.player == player) return it
+        }
+        return null
+    }
+
+    fun endAllCrates() {
+        QuadCrateSession.getCrateSessions().forEach { session -> session.endCrateForce(false) }
+        QuadCrateSession.getCrateSessions().clear()
+    }
+}
