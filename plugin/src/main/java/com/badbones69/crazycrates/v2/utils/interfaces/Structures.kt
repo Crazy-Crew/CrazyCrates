0582e5d089e2fb571f99773d3bfa83cf3fcc9a3a
Ryder Belserion <RyderBelserion@protonmail.com>
2022-04-04 16:13:36 -0400

    Update interface

--- plugin/src/main/java/com/badbones69/crazycrates/v2/utils/interfaces/Structures.kt
+++ plugin/src/main/java/com/badbones69/crazycrates/v2/utils/interfaces/Structures.kt
@@ -3,23 +3,24 @@ package com.badbones69.crazycrates.v2.utils.interfaces
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.block.Block
+import org.bukkit.entity.Player
 
 interface Structures {
 
     // Paste a structure using available .nbt files
-    fun pasteStructure(location: Location)
+    fun pasteStructure(location: Location, player: Player)
 
     // Save a new nbt structure
-    fun saveStructure(location: Array<Location>)
+    fun saveStructure(location: ArrayList<Location>)
 
     // Remove a structure
-    fun removeStructure(location: Array<Location>)
+    fun removeStructure(location: Location)
 
     // Add & Get the blocks from/to the ArrayList
     fun getStructureBlocks(location: Location): ArrayList<Block>
 
     // Get blocks prior to spawning in the structure
-    fun getNearbyBlocks(location: Location): List<Block>
+    fun getNearbyBlocks(location: Location): ArrayList<Block>
 
     // Get a list of black-listed blocks
     fun getBlackList(): List<Material>
