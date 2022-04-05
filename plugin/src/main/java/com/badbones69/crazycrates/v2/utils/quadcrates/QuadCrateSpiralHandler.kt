9245abf09eceac4dfeb97de3f9b320aa386ddeec
Ryder Belserion <RyderBelserion@protonmail.com>
2022-04-04 22:42:19 -0400

    Use cos/sin instead of Math.sin/cos

--- plugin/src/main/java/com/badbones69/crazycrates/v2/utils/quadcrates/QuadCrateSpiralHandler.kt
+++ plugin/src/main/java/com/badbones69/crazycrates/v2/utils/quadcrates/QuadCrateSpiralHandler.kt
@@ -2,6 +2,8 @@ package com.badbones69.crazycrates.v2.utils.quadcrates
 
 import com.badbones69.crazycrates.v2.utils.interfaces.SpiralControl
 import org.bukkit.Location
+import kotlin.math.cos
+import kotlin.math.sin
 
 class QuadCrateSpiralHandler: SpiralControl {
     override fun getSpiralLocationClockwise(center: Location): ArrayList<Location> {
@@ -20,16 +22,18 @@ class QuadCrateSpiralHandler: SpiralControl {
         val locations = java.util.ArrayList<Location>()
         for (i in 0..59) {
             val angle = nextLocation * increment
-            val x = center.x + radius * Math.cos(angle)
-            val z = center.z + radius * Math.sin(angle)
+            val x = center.x + radius * cos(angle)
+            val z = center.z + radius * sin(angle)
             locations.add(Location(world, x, y, z))
             y -= downwardsDistance
             nextLocation++
             increaseRadius++
+
             if (increaseRadius == 6) {
                 increaseRadius = 0
                 radius += expandingRadius
             }
+
             if (nextLocation == 10) {
                 nextLocation = 0
             }
@@ -53,16 +57,18 @@ class QuadCrateSpiralHandler: SpiralControl {
         val locations = java.util.ArrayList<Location>()
         for (i in 0..59) {
             val angle = nextLocation * increment
-            val x = center.x - radius * Math.cos(angle)
-            val z = center.z - radius * Math.sin(angle)
+            val x = center.x - radius * cos(angle)
+            val z = center.z - radius * sin(angle)
             locations.add(Location(world, x, y, z))
             y -= downwardsDistance
             nextLocation++
             increaseRadius++
+
             if (increaseRadius == 6) {
                 increaseRadius = 0
                 radius += expandingRadius
             }
+
             if (nextLocation == 10) {
                 nextLocation = 0
             }
