270a97d5089a169d7ff82b0464fde4eb29b73e2b
Ryder Belserion <RyderBelserion@protonmail.com>
2022-04-07 21:44:25 -0400

    Test command for structures

---
+++ plugin/src/main/java/com/badbones69/crazycrates/commands/v2/TestCommand.kt
@@ -0,0 +1,23 @@
+package com.badbones69.crazycrates.commands.v2
+
+import com.badbones69.crazycrates.getPlugin
+import com.badbones69.crazycrates.v2.utils.quadcrates.StructureHandler
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import java.io.File
+
+object TestCommand : CommandExecutor {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
+
+        val player = sender as Player
+
+        val file = File("${getPlugin().dataFolder}/schematics/classic.nbt")
+
+        StructureHandler(file).pasteStructure(player.location)
+
+        return false
+    }
+}
