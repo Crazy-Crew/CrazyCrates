70182c3f4974b7b91d70ddf271dfd0a6a4b1ee8f
Ryder Belserion <RyderBelserion@protonmail.com>
2022-04-10 11:35:57 -0400

    Move copyFile into a util class

---
+++ plugin/src/main/java/com/badbones69/crazycrates/v2/utils/FileUtil.kt
@@ -0,0 +1,20 @@
+package com.badbones69.crazycrates.v2.utils
+
+import java.io.File
+import java.io.InputStream
+
+object FileUtil {
+    fun copyFile(jarFile: InputStream, file: File) {
+        runCatching {
+            jarFile.use { inStream ->
+                file.outputStream().use { outStream ->
+                    val buffer = ByteArray(1024)
+                    var count: Int
+                    while (inStream.read(buffer).also { count = it } != -1) outStream.write(buffer, 0, count)
+                }
+            }
+        }.onFailure {
+            println("Saving ${file.name} failed. \n ${it.message}")
+        }
+    }
+}
