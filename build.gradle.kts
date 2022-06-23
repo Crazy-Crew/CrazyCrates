2918c5488634bd894eeca5cb9bf3702cd44ea601
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-06-23 12:28:41 -0400

    Update versioning.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ tasks.compileKotlin {
 }
 
 group = "com.badbones69.crazycrates"
-version = "1.19-1.10.5-${System.getenv("BUILD_NUMBER") ?: "SNAPSHOT"}"
+version = "[1.18-1.19]-1.10.5-${System.getenv("BUILD_NUMBER") ?: "SNAPSHOT"}"
 description = "Quality crates for free!"
 
 repositories {
