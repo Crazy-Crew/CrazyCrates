0860dd0756b1392c9ad52570d7ee194e396dc214
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-07-13 23:22:52 -0400

    Update CrazyCrates.java

--- src/main/java/com/badbones69/crazycrates/CrazyCrates.java
+++ src/main/java/com/badbones69/crazycrates/CrazyCrates.java
@@ -57,8 +57,8 @@ public class CrazyCrates extends JavaPlugin implements Listener {
             getLogger().warning(" bug fixes, security enhancements and optional");
             getLogger().warning(" features for server owners to enhance their server.");
             getLogger().warning(" ");
-            getLogger().warning(" All of your plugins should still work, and the");
-            getLogger().warning(" Paper community will gladly help you fix any issues.");
+            getLogger().warning(" All of your plugins will function the same,");
+            getLogger().warning(" as it is a drop in replacement over spigot.")
             getLogger().warning("");
             getLogger().warning(" Join the Purpur Community @ https://purpurmc.org/discord");
             getLogger().warning("====================================================");
