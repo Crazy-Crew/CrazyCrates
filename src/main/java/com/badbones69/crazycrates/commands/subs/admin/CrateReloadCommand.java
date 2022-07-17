39a6c3d4a88d9f28c58f0d912c92e40337f44e73
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-07-16 22:35:23 -0400

    Basic commands

---
+++ src/main/java/com/badbones69/crazycrates/commands/subs/admin/CrateReloadCommand.java
@@ -0,0 +1,21 @@
+package com.badbones69.crazycrates.commands.subs.admin;
+
+import com.badbones69.crazycrates.api.FileManager;
+import com.badbones69.crazycrates.api.enums.settings.Messages;
+import com.badbones69.crazycrates.commands.CrateBaseCommand;
+import dev.triumphteam.cmd.bukkit.annotation.Permission;
+import dev.triumphteam.cmd.core.annotation.SubCommand;
+import org.bukkit.command.CommandSender;
+
+public class CrateReloadCommand extends CrateBaseCommand {
+
+    private final FileManager fileManager = FileManager.getInstance();
+
+    @SubCommand("reload")
+    @Permission("crazycrates.command.admin.reload")
+    public void reload(CommandSender sender) {
+        fileManager.reloadAllFiles();
+
+        sender.sendMessage(Messages.RELOAD.getMessage());
+    }
+}
