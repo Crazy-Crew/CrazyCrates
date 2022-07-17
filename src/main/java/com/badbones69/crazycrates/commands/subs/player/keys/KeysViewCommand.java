233402fe4c59e0d03c1d1800d203a923d4015c23
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-07-16 22:34:50 -0400

    Simplified key commands

---
+++ src/main/java/com/badbones69/crazycrates/commands/subs/player/keys/KeysViewCommand.java
@@ -0,0 +1,21 @@
+package com.badbones69.crazycrates.commands.subs.player.keys;
+
+import com.badbones69.crazycrates.api.enums.settings.Messages;
+import com.badbones69.crazycrates.commands.KeysBaseCommand;
+import com.badbones69.crazycrates.support.utils.KeyUtils;
+import dev.triumphteam.cmd.bukkit.annotation.Permission;
+import dev.triumphteam.cmd.core.annotation.SubCommand;
+import org.bukkit.entity.Player;
+
+public class KeysViewCommand extends KeysBaseCommand {
+
+    private final KeyUtils keyUtils = KeyUtils.getKeyUtils();
+
+    @SubCommand("view")
+    @Permission("crazycrates.command.player.key.others")
+    public void viewOthers(Player player, Player target) {
+        String header = Messages.OTHER_PLAYER_HEADER.getMessageNoPrefix().replace("%player%", target.getName());
+
+        keyUtils.checkKeys(target, header, player);
+    }
+}
---
+++ src/main/java/com/badbones69/crazycrates/support/utils/KeyUtils.java
@@ -0,0 +1,65 @@
+package com.badbones69.crazycrates.support.utils;
+
+import com.badbones69.crazycrates.api.CrazyManager;
+import com.badbones69.crazycrates.api.enums.settings.Messages;
+import com.badbones69.crazycrates.api.objects.Crate;
+import org.bukkit.entity.Player;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class KeyUtils {
+
+    private final CrazyManager crazyManager = CrazyManager.getInstance();
+
+    private static final KeyUtils keyUtils = new KeyUtils();
+
+    public static KeyUtils getKeyUtils() {
+        return keyUtils;
+    }
+
+    public void checkKeys(Player player, String messageHeader, Player otherPlayer) {
+
+        AtomicBoolean hasKeys = new AtomicBoolean(false);
+
+        HashMap<Crate, Integer> keys = crazyManager.getVirtualKeys(player);
+        ArrayList<String> messages = new ArrayList<>();
+        messages.add(messageHeader);
+
+        keys.keySet().forEach(crate -> {
+            int amount = keys.get(crate);
+
+            if (amount > 0) {
+                hasKeys.set(true);
+                HashMap<String, String> placeholders = new HashMap<>();
+                placeholders.put("%Crate%", crate.getFile().getString("Crate.Name"));
+                placeholders.put("%Keys%", amount + "");
+                messages.add(Messages.PER_CRATE.getMessageNoPrefix(placeholders));
+            }
+        });
+
+        if (hasKeys.get()) {
+            if (otherPlayer != null) {
+
+                if (otherPlayer.getUniqueId() == player.getUniqueId()) {
+                    player.sendMessage(Messages.SAME_PLAYER.getMessage());
+                    return;
+                }
+
+                otherPlayer.sendMessage(Messages.convertList(messages));
+                return;
+            }
+
+            player.sendMessage(Messages.convertList(messages));
+
+            return;
+        }
+
+        if (otherPlayer != null) {
+            otherPlayer.sendMessage(Messages.OTHER_PLAYER_NO_VIRTUAL_KEYS.getMessage("%player%", player.getName()));
+            return;
+        }
+
+        player.sendMessage(Messages.PERSONAL_NO_VIRTUAL_KEYS.getMessage());
+    }
+}
