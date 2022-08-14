060a7924c113dc4e134c4f44b6f2a1480ed8999a
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-08-14 00:25:56 -0400

    Unfinished config binder?

---
+++ paper/src/main/java/com/badbones69/crazycrates/modules/config/ConfigBinder.java
@@ -0,0 +1,16 @@
+package com.badbones69.crazycrates.modules.config;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+
+public class ConfigBinder extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bindConfig(new ConfigFile(), "config");
+    }
+
+    private void bindConfig(ConfigFile config, String name) {
+        bind(ConfigFile.class).annotatedWith(Names.named(name)).toInstance(config);
+    }
+}
