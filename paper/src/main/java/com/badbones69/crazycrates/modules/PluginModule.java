5c0f86a916972c75c3ee11fa25d1c5823581f304
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-08-14 00:25:47 -0400

    Add functions class

--- paper/src/main/java/com/badbones69/crazycrates/modules/PluginModule.java
+++ paper/src/main/java/com/badbones69/crazycrates/modules/PluginModule.java
@@ -4,10 +4,14 @@ import com.badbones69.crazycrates.CrazyCrates;
 import com.badbones69.crazycrates.Methods;
 import com.badbones69.crazycrates.api.CrazyManager;
 import com.badbones69.crazycrates.api.FileManager;
+import com.badbones69.crazycrates.utilities.Functions;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.name.Names;
+
 import javax.annotation.Nonnull;
+import java.io.File;
 
 public class PluginModule extends AbstractModule {
 
@@ -16,7 +20,10 @@ public class PluginModule extends AbstractModule {
     private final CrazyManager crazyManager = new CrazyManager();
     private final FileManager fileManager = new FileManager();
 
+    // Old methods class.
     private final Methods methods = new Methods();
+    // New functions class to replace Methods slowly.
+    private final Functions functions = new Functions();
 
     public PluginModule(CrazyCrates plugin) {
         this.plugin = plugin;
@@ -36,5 +43,8 @@ public class PluginModule extends AbstractModule {
         bind(FileManager.class).toInstance(fileManager);
 
         bind(Methods.class).toInstance(methods);
+        bind(Functions.class).toInstance(functions);
+
+        bind(File.class).annotatedWith(Names.named("ConfigFolder")).toInstance(plugin.getDataFolder());
     }
 }
---
+++ paper/src/main/java/com/badbones69/crazycrates/utilities/Functions.java
@@ -0,0 +1,27 @@
+package com.badbones69.crazycrates.utilities;
+
+import com.badbones69.crazycrates.CrazyCrates;
+import com.google.inject.Inject;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class Functions {
+
+    @Inject private CrazyCrates plugin;
+
+    public void copyFile(InputStream sourceFile, File destinationFile) {
+        try (InputStream fis = sourceFile; FileOutputStream fos = new FileOutputStream(destinationFile)) {
+            byte[] buf = new byte[1024];
+            int i;
+
+            while ((i = fis.read(buf)) != -1) {
+                fos.write(buf, 0, i);
+            }
+        } catch (Exception e) {
+            plugin.getLogger().warning("Failed to copy " + destinationFile.getName() + "...");
+
+            e.printStackTrace();
+        }
+    }
+}
