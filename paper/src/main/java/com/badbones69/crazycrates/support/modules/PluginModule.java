c8a8ffddfbdceb3525a168463df402f73232251b
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-08-13 23:02:42 -0400

    Add guice

---
+++ paper/src/main/java/com/badbones69/crazycrates/support/modules/PluginModule.java
@@ -0,0 +1,35 @@
+package com.badbones69.crazycrates.support.modules;
+
+import com.badbones69.crazycrates.CrazyCrates;
+import com.badbones69.crazycrates.api.CrazyManager;
+import com.badbones69.crazycrates.api.FileManager;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import javax.annotation.Nonnull;
+
+public class PluginModule extends AbstractModule {
+
+    private final CrazyCrates plugin;
+
+    private final CrazyManager crazyManager = new CrazyManager();
+    private final FileManager fileManager = new FileManager();
+
+    public PluginModule(CrazyCrates plugin) {
+        this.plugin = plugin;
+    }
+
+    @Nonnull
+    public Injector createInjector() {
+        return Guice.createInjector(this);
+    }
+
+    @Override
+    protected void configure() {
+        bind(CrazyCrates.class).toInstance(plugin);
+
+        bind(CrazyManager.class).toInstance(crazyManager);
+
+        bind(FileManager.class).toInstance(fileManager);
+    }
+}
