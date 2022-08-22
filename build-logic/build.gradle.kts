7b12bcf63c6c220d18dfc7ee6f6ef94a24a7bbca
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-08-22 18:17:34 -0400

    Downgrade build script

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,5 +7,5 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20-Beta")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
 }
