93f2b7f1d937c0a34078447677d9d6ce2982d979
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-09-18 20:30:29 -0400

    Modrinth publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,35 @@
 plugins {
     id("crazycrates-base")
+
+    id("com.modrinth.minotaur") version "2.+"
+    id("co.uzzu.dotenv.gradle") version "2.0.0"
+}
+
+tasks {
+    modrinth {
+        token.set(env.MODRINTH_TOKEN.value)
+        projectId.set("crazycrates")
+        versionName.set("${rootProject.version}")
+        versionNumber.set("${rootProject.version}")
+
+        versionType.set("alpha")
+
+        /**
+         * Uncomment which one you are uploading Badbones.
+         * Default is PAPER
+         */
+        uploadFile.set(jar(rootProject.name, "PAPER"))
+        //uploadFile.set(jar(rootProject.name, "SPONGE"))
+        //uploadFile.set(jar(rootProject.name, "FABRIC"))
+
+        gameVersions.addAll(listOf("1.19.2"))
+
+        loaders.addAll(listOf("paper", "purpur"))
+
+        changelog.set(System.getenv("COMMIT_MESSAGE"))
+    }
+}
+
+fun jar(name: String, platform: String): RegularFile {
+    return rootProject.layout.buildDirectory.file("libs/${name}-${rootProject.version}-${platform}.jar").get()
 }
