2dd0c4e99d533ec41fb9b45d7e152a0470175bc8
Ryder Belserion <no-reply@ryderbelserion.com>
2022-12-21 16:25:40 -0500

    [ci-skip] Remove dead import

--- paper/src/main/java/com/badbones69/crazycrates/CrazyCrates.java
+++ paper/src/main/java/com/badbones69/crazycrates/CrazyCrates.java
@@ -39,8 +39,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-
 import java.util.ArrayList;
 import java.util.List;
 
