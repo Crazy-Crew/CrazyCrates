19ce68c54a203e818978e20540c40302f5c3f862
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2023-02-06 02:15:03 -0500

    [ci-skip] Set up exclusiveContent for papermc.io

--- build-logic/src/main/kotlin/crazycrates.paper-plugin.gradle.kts
+++ build-logic/src/main/kotlin/crazycrates.paper-plugin.gradle.kts
@@ -3,5 +3,17 @@ plugins {
 }
 
 repositories {
-    maven("https://repo.papermc.io/repository/maven-public/")
+    repositories {
+        exclusiveContent {
+            forRepository {
+                maven("https://repo.papermc.io/repository/maven-public/")
+            }
+
+            filter {
+                includeGroup("io.papermc.paper")
+                includeGroup("com.mojang")
+                includeGroup("net.md-5")
+            }
+        }
+    }
 }
