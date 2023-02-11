2727b80677521e039743f97b9bd1243345db7008
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2023-02-11 03:19:15 -0500

    Simplify file utils

--- core/src/main/java/com/badbones69/crazycrates/utils/FileUtils.java
+++ core/src/main/java/com/badbones69/crazycrates/utils/FileUtils.java
@@ -3,31 +3,13 @@ package com.badbones69.crazycrates.utils;
 import com.badbones69.crazycrates.utils.adventure.MsgWrapper;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class FileUtils {
 
-    public static void copyFile(File input, File output, org.simpleyaml.configuration.file.YamlConfiguration configuration, Path directory) {
+    public static void copyFile(File input, File output, org.simpleyaml.configuration.file.YamlFile configuration) {
         try {
-            if (output.exists()) {
-                File updateDir = new File(directory + "/updates");
-
-                String name = output.getName().replace(".yml", "").replace("v1", "");
-
-                String fileName = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
-
-                File newOutput = new File(name + fileName + ".yml");
-
-                if (output.renameTo(newOutput)) MsgWrapper.send("<#E0115F>Successfully added time stamp to <#11e092>" + output.getName() + ".");
-
-                Files.move(newOutput.toPath(), Path.of(updateDir.toPath() + "/" + newOutput.getName()));
-
-                MsgWrapper.send("<#E0115F>Successfully moved <#11e092>" + newOutput.getName() + " <#E0115F>to <#11e092>" + updateDir.getName());
-            }
-
+            if (output.exists()) if (output.delete()) MsgWrapper.send("<#E0115F>Deleted <#11e092>" + output.getName() + " <#E0115F>because we have copied it.");
+            
             configuration.save(input);
         } catch (IOException e) {
             MsgWrapper.send(e.getMessage());
