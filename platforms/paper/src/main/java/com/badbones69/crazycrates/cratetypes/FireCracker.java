9d2fec23fb4185ea544f28d675d7f302ff318a95
BadBones <joewojcik14@gmail.com>
2023-02-18 17:36:23 -0500

    Cleaned up FireCracker class.

--- platforms/paper/src/main/java/com/badbones69/crazycrates/cratetypes/FireCracker.java
+++ platforms/paper/src/main/java/com/badbones69/crazycrates/cratetypes/FireCracker.java
@@ -19,8 +19,10 @@ public class FireCracker implements CrateManager {
 
     private final List<Color> colors = Arrays.asList(Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE, Color.BLACK, Color.AQUA, Color.MAROON, Color.PURPLE);
 
+    private final Random random = new Random();
+
     public void openCrate(Player player, Crate crate, KeyType keyType, boolean checkHand) {
-        Location loc = CrateControlListener.inUse.get(player);
+        Location originalLocation = CrateControlListener.inUse.get(player);
         HologramController hologramController = crazyManager.getHologramController();
         if (!crazyManager.takeKeys(1, player, crate, keyType, true)) {
             Methods.failedToTakeKey(player, crate);
@@ -28,27 +30,30 @@ public class FireCracker implements CrateManager {
             return;
         }
 
-        if (hologramController != null) hologramController.removeHologram(loc.getBlock());
+        if (hologramController != null) hologramController.removeHologram(originalLocation.getBlock());
 
         crazyManager.addCrateTask(player, new BukkitRunnable() {
-            final Random r = new Random();
-            final int color = r.nextInt(colors.size());
-            int l = 0;
-            final Location L = loc.clone().add(.5, 25, .5);
+            final Color color = getRandomColor();
+            int level = 0;
+            final Location fireworkLocation = originalLocation.clone().add(.5, 25, .5);
 
             @Override
             public void run() {
-                L.subtract(0, 1, 0);
-                Methods.firework(L, colors.get(color));
-                l++;
+                fireworkLocation.subtract(0, 1, 0);
+                Methods.firework(fireworkLocation, color);
+                level++;
 
-                if (l == 25) {
+                if (level == 25) {
                     crazyManager.endCrate(player);
                     // The key type is set to free because the key has already been taken above.
                     new QuickCrate().openCrate(player, crate, KeyType.FREE_KEY, checkHand);
                 }
             }
         }.runTaskTimer(plugin, 0, 2));
+    }
+
+    private Color getRandomColor() {
+        return colors.get(random.nextInt(colors.size()));
     }
 
 }
