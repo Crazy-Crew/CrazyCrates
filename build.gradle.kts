259b38fc973537eb1c8f1451d214cf8477004a82
Ryder Belserion <no-reply@ryderbelserion.com>
2023-04-12 23:30:36 -0400

    Download link has to be set after the version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import com.lordcodes.turtle.shellRun
 import task.WebhookExtension
 import java.awt.Color
 
@@ -18,15 +17,11 @@ val extension = settings.versions.extension.get()
 val color = if (beta) betaUpdate else releaseUpdate
 val repo = if (beta) "beta" else "releases"
 
-val download = "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
-
 val msg = "New version of ${rootProject.name} is ready! <@&929463441159254066>"
-
-val hash = shellRun("git", listOf("rev-parse", "--short", "HEAD"))
 
 rootProject.version = "1.11.15.3"
 
-val desc = "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
+val download = "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
 
 webhook {
     this.avatar("https://en.gravatar.com/avatar/${WebhookExtension.Gravatar().md5Hex("no-reply@ryderbelserion.com")}.jpeg")
@@ -70,7 +65,7 @@ webhook {
                 > The importer is not 100%, Please manually check your configs and tweak things accordingly
                 > You might have to switch some things over to using commands if it could not be converted.
                 
-                Full Changelog -> $desc
+                Full Changelog -> $download
             """.trimIndent())
         }
     }
