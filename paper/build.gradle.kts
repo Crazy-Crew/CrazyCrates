802c109da3bf201d3b9aaf4da310452c54140be3
Ryder Belserion <no-reply@ryderbelserion.com>
2023-09-22 15:12:41 -0400

    Bump nbt-api

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 
     implementation("org.bstats", "bstats-bukkit", "3.0.2")
 
-    implementation("de.tr7zw", "item-nbt-api", "2.11.3")
+    implementation("de.tr7zw", "item-nbt-api", "2.12.0")
 
     implementation("dev.triumphteam", "triumph-cmd-bukkit", "2.0.0-SNAPSHOT")
 
