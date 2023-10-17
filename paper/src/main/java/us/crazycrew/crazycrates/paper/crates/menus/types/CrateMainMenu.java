cc69458e971b812ef4d2d1520ad9882a267100f6
Ryder Belserion <no-reply@ryderbelserion.com>
2023-10-17 19:31:11 -0400

    Add filler options

--- paper/src/main/java/us/crazycrew/crazycrates/paper/crates/menus/types/CrateMainMenu.java
+++ paper/src/main/java/us/crazycrew/crazycrates/paper/crates/menus/types/CrateMainMenu.java
@@ -1,23 +1,32 @@
 package us.crazycrew.crazycrates.paper.crates.menus.types;
 
+import ch.jalu.configme.SettingsManager;
 import com.ryderbelserion.cluster.paper.items.NbtBuilder;
 import com.ryderbelserion.cluster.paper.items.ParentBuilder;
 import dev.triumphteam.gui.builder.item.ItemBuilder;
+import dev.triumphteam.gui.components.GuiType;
+import dev.triumphteam.gui.components.util.GuiFiller;
 import dev.triumphteam.gui.guis.Gui;
 import dev.triumphteam.gui.guis.GuiItem;
 import org.bukkit.inventory.ItemStack;
+import us.crazycrew.crazycrates.common.config.types.menus.MainMenuConfig;
 import us.crazycrew.crazycrates.paper.CrazyCrates;
 import us.crazycrew.crazycrates.paper.crates.object.Crate;
+
+import java.util.List;
 import java.util.Set;
 
 public class CrateMainMenu {
 
     private final CrazyCrates plugin;
 
+    private final SettingsManager mainMenuConfig;
     private final Set<Crate> crateConfigs;
 
     public CrateMainMenu(CrazyCrates plugin) {
         this.plugin = plugin;
+
+        this.mainMenuConfig = this.plugin.getConfigManager().getMainMenuConfig();
 
         this.crateConfigs = this.plugin.getCrazyHandler().getCrateManager().getCrates();
     }
@@ -26,10 +35,25 @@ public class CrateMainMenu {
 
     public void create() {
         this.gui = Gui.gui()
-                .title(this.plugin.getCrazyHandler().parse("<red>Main Menu"))
+                .title(this.plugin.getCrazyHandler().parse(this.mainMenuConfig.getProperty(MainMenuConfig.crate_menu_title)))
                 .disableAllInteractions()
-                .rows(6)
+                .type(GuiType.valueOf(this.mainMenuConfig.getProperty(MainMenuConfig.crate_menu_type)))
+                .rows(this.mainMenuConfig.getProperty(MainMenuConfig.crate_menu_rows))
                 .create();
+
+        if (this.mainMenuConfig.getProperty(MainMenuConfig.crate_menu_filler_toggle)) {
+            String item = this.mainMenuConfig.getProperty(MainMenuConfig.crate_menu_filler_item);
+            String name = this.mainMenuConfig.getProperty(MainMenuConfig.crate_menu_filler_name);
+            List<String> lore = this.mainMenuConfig.getProperty(MainMenuConfig.crate_menu_filler_lore);
+
+            GuiFiller guiFiller = this.gui.getFiller();
+
+            ItemStack itemStack = ParentBuilder.of(this.plugin).setMaterial(item).setDisplayName(name).setDisplayLore(lore).build();
+
+            GuiItem guiItem = new GuiItem(itemStack);
+
+            guiFiller.fill(guiItem);
+        }
 
         if (this.crateConfigs.isEmpty()) return;
 
