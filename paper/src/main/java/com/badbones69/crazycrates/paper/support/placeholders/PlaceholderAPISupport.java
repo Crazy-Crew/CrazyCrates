c89c28a65f7527dfcbca0bae547ed82415108c1a
Ryder Belserion <no-reply@ryderbelserion.com>
2023-10-22 15:49:13 -0400

    Add v2 placeholders for placeholder api
    v1 placeholders are still there

--- paper/src/main/java/com/badbones69/crazycrates/paper/support/placeholders/PlaceholderAPISupport.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/support/placeholders/PlaceholderAPISupport.java
@@ -8,8 +8,10 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
 import java.text.NumberFormat;
 
+@SuppressWarnings("deprecation")
 public class PlaceholderAPISupport extends PlaceholderExpansion {
 
     private final CrazyCrates plugin = CrazyCrates.getPlugin();
@@ -18,34 +20,90 @@ public class PlaceholderAPISupport extends PlaceholderExpansion {
     
     @Override
     public String onRequest(OfflinePlayer player, @NotNull String identifier) {
-        if (player.isOnline()) {
-            Player playerOnline = (Player) player;
+        if (player == null) {
+            return "N/A";
+        }
 
-            for (Crate crate : crazyManager.getCrates()) {
-                if (crate.getCrateType() != CrateType.MENU) {
-                    if (identifier.equalsIgnoreCase(crate.getName())) {
-                        return NumberFormat.getNumberInstance().format(crazyManager.getVirtualKeys(playerOnline, crate));
-                    } else if (identifier.equalsIgnoreCase(crate.getName() + "_physical")) {
-                        return NumberFormat.getNumberInstance().format(crazyManager.getPhysicalKeys(playerOnline, crate));
-                    } else if (identifier.equalsIgnoreCase(crate.getName() + "_total")) {
-                        return NumberFormat.getNumberInstance().format(crazyManager.getTotalKeys(playerOnline, crate));
-                    } else if (identifier.equalsIgnoreCase(crate.getName() + "_opened")) { // crazycrates_crateexample_opened
-                        return NumberFormat.getNumberInstance().format(crazyManager.getCratesOpened(playerOnline, crate));
-                    } else if (identifier.equalsIgnoreCase("crates_opened")) { // crazycrates_crates_opened
-                        return NumberFormat.getNumberInstance().format(crazyManager.getTotalCratesOpened(playerOnline));
-                    }
+        // The player who sees the placeholder.
+        Player human = (Player) player;
+
+        for (Crate crate : this.crazyManager.getCrates()) {
+            if (crate.getCrateType() != CrateType.MENU) {
+                if (identifier.equalsIgnoreCase(crate.getName())) {
+                    return NumberFormat.getNumberInstance().format(this.crazyManager.getVirtualKeys(human, crate));
+                }
+
+                if (identifier.equalsIgnoreCase(crate.getName() + "_physical")) {
+                    return NumberFormat.getNumberInstance().format(this.crazyManager.getPhysicalKeys(human, crate));
+                }
+
+                if (identifier.equalsIgnoreCase(crate.getName() + "_total")) {
+                    return NumberFormat.getNumberInstance().format(this.crazyManager.getTotalKeys(human, crate));
+                }
+
+                if (identifier.equalsIgnoreCase(crate.getName() + "_opened")) {
+                    return NumberFormat.getNumberInstance().format(this.crazyManager.getCratesOpened(human, crate));
+                }
+
+                if (identifier.equalsIgnoreCase("crates_opened")) {
+                    return NumberFormat.getNumberInstance().format(this.crazyManager.getTotalCratesOpened(human));
                 }
             }
         }
 
-        return "";
+        // Get the player name online or offline i.e <player>
+        String playerName = identifier.split("_")[0];
+
+        Player target = this.plugin.getServer().getPlayer(playerName);
+
+        if (target == null) {
+            this.plugin.getLogger().warning("Player: " + playerName + " is likely not online or doesn't exist.");
+            return "N/A";
+        }
+
+        if (identifier.equalsIgnoreCase(target.getName() + "_opened")) { // %crazycrates_<player>_opened%
+            return NumberFormat.getNumberInstance().format(this.crazyManager.getTotalCratesOpened(target));
+        }
+
+        // Get the crate name i.e <crate>
+        String crateName = identifier.split("_")[1];
+
+        Crate crate = this.crazyManager.getCrateFromName(crateName);
+
+        if (crate == null) {
+            this.plugin.getLogger().warning("Crate: " + crateName + " is not a valid crate name.");
+            return "N/A";
+        }
+
+        if (identifier.equalsIgnoreCase(target.getName() + "_" + crate.getName() + "_total")) { // %crazycrates_<player>_<crate>_total%
+            return NumberFormat.getNumberInstance().format(this.crazyManager.getTotalKeys(target, crate));
+        }
+
+        if (identifier.equalsIgnoreCase(target.getName() + "_" + crate.getName() + "_physical")) { // %crazycrates_<player>_<crate>_physical%
+            return NumberFormat.getNumberInstance().format(this.crazyManager.getPhysicalKeys(target, crate));
+        }
+
+        if (identifier.equalsIgnoreCase(target.getName() + "_" + crate.getName() + "_virtual")) { // %crazycrates_<player>_<crate>_virtual%
+            return NumberFormat.getNumberInstance().format(this.crazyManager.getVirtualKeys(target, crate));
+        }
+
+        if (identifier.equalsIgnoreCase(target.getName() + "_" + crate.getName() + "_opened")) { // %crazycrates_<player>_<crate>_opened%
+            return NumberFormat.getNumberInstance().format(this.crazyManager.getCratesOpened(target, crate));
+        }
+
+        return "N/A";
     }
     
     @Override
     public boolean persist() {
         return true;
     }
-    
+
+    @Override
+    public boolean canRegister() {
+        return true;
+    }
+
     @Override
     public @NotNull String getIdentifier() {
         return "crazycrates";
@@ -53,11 +111,11 @@ public class PlaceholderAPISupport extends PlaceholderExpansion {
     
     @Override
     public @NotNull String getAuthor() {
-        return "BadBones69";
+        return this.plugin.getDescription().getAuthors().toString();
     }
     
     @Override
     public @NotNull String getVersion() {
-        return plugin.getDescription().getVersion();
+        return this.plugin.getDescription().getVersion();
     }
 }
