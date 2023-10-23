e96031a0fcb6f32e4d6c56fbdf9e2ed8e2d8fc4b
Ryder Belserion <no-reply@ryderbelserion.com>
2023-10-23 13:15:27 -0400

    Add extra metric charts

--- paper/src/main/java/us/crazycrew/crazycrates/paper/api/support/metrics/MetricsWrapper.java
+++ paper/src/main/java/us/crazycrew/crazycrates/paper/api/support/metrics/MetricsWrapper.java
@@ -1,26 +1,51 @@
 package us.crazycrew.crazycrates.paper.api.support.metrics;
 
 import com.badbones69.crazycrates.paper.CrazyCrates;
+import com.badbones69.crazycrates.paper.api.CrazyManager;
 import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import us.crazycrew.crazycrates.api.enums.types.CrateType;
 
 public class MetricsWrapper {
 
     @NotNull
     private final CrazyCrates plugin = JavaPlugin.getPlugin(CrazyCrates.class);
 
+    @NotNull
+    private final CrazyManager crazyManager = this.plugin.getStarter().getCrazyManager();
+
+    private Metrics metrics;
+
     public void start() {
-        new Metrics(this.plugin, 4514);
+        if (this.metrics != null) {
+            if (this.plugin.isLogging()) this.plugin.getLogger().warning("Metrics is already enabled.");
+            return;
+        }
 
-        //crazyManager.getCrates().forEach(crate -> {
-       //     CrateType crateType = crate.getCrateType();
+        this.metrics = new Metrics(this.plugin, 4514);
 
-        //    SimplePie crateChart = new SimplePie("crate_types", crateType::getName);
+        this.crazyManager.getCrates().forEach(crate -> {
+            CrateType crateType = crate.getCrateType();
 
-        //    metrics.addCustomChart(crateChart);
-        //});
+            SimplePie chart = new SimplePie("crate_types", crateType::getName);
 
-        this.plugin.getLogger().info("Metrics has been enabled.");
+            this.metrics.addCustomChart(chart);
+        });
+
+        if (this.plugin.isLogging()) this.plugin.getLogger().fine("Metrics has been enabled.");
+    }
+
+    public void stop() {
+        if (this.metrics == null) {
+            if (this.plugin.isLogging()) this.plugin.getLogger().warning("Metrics isn't enabled so we do nothing.");
+            return;
+        }
+
+        this.metrics.shutdown();
+        this.metrics = null;
+
+        if (this.plugin.isLogging()) this.plugin.getLogger().fine("Metrics has been turned off.");
     }
 }
