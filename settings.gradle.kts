196b656e5a3bdc905b4eacfce1f0b9970cbe80c3
Ryder Belserion <no-reply@ryderbelserion.com>
2023-10-22 22:18:17 -0400

    Update included projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,14 @@ pluginManagement {
 rootProject.name = "CrazyCrates"
 
 listOf(
-    "core",
+    // v2
+    "api",
+    "common",
+
     "paper",
+
+    // v1
+    "core",
 ).forEach {
     include(it)
 }
