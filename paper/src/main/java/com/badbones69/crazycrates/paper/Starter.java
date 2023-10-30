4372e47ca28293f6a5f5eaee8f83392122223fd3
Ryder Belserion <no-reply@ryderbelserion.com>
2023-10-30 14:15:54 -0400

    This was moved

--- paper/src/main/java/com/badbones69/crazycrates/paper/Starter.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/Starter.java
@@ -2,31 +2,22 @@ package com.badbones69.crazycrates.paper;
 
 import com.badbones69.crazycrates.paper.api.CrazyManager;
 import com.badbones69.crazycrates.paper.api.EventLogger;
-import us.crazycrew.crazycrates.paper.support.structures.blocks.ChestStateHandler;
 
 @Deprecated(since = "1.16", forRemoval = true)
 public class Starter {
 
     private CrazyManager crazyManager;
 
-    private ChestStateHandler chestStateHandler;
-
     private EventLogger eventLogger;
 
     public void run() {
         this.crazyManager = new CrazyManager();
-
-        this.chestStateHandler = new ChestStateHandler();
 
         this.eventLogger = new EventLogger();
     }
 
     public CrazyManager getCrazyManager() {
         return this.crazyManager;
-    }
-
-    public ChestStateHandler getChestStateHandler() {
-        return this.chestStateHandler;
     }
 
     public EventLogger getEventLogger() {
