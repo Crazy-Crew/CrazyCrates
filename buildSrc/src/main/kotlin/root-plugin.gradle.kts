501e77c4c6de43cd091a195a07773a6a9c9237f9
Ryder Belserion <no-reply@ryderbelserion.com>
2023-11-04 15:42:56 -0400

    [ci-skip] api doesn't need snapshots

--- buildSrc/src/main/kotlin/root-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/root-plugin.gradle.kts
@@ -32,19 +32,12 @@ tasks {
     }
 }
 
-val isSnapshot = rootProject.version.toString().contains("snapshot")
-
 publishing {
     repositories {
         maven {
             credentials {
                 this.username = System.getenv("gradle_username")
                 this.password = System.getenv("gradle_password")
-            }
-
-            if (isSnapshot) {
-                url = uri("https://repo.crazycrew.us/snapshots/")
-                return@maven
             }
 
             url = uri("https://repo.crazycrew.us/releases/")
