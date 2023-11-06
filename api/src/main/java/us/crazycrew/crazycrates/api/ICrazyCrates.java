e40b96f3e8c5b155778b972765ebd9c515f3b670
Ryder Belserion <no-reply@ryderbelserion.com>
2023-11-05 19:39:32 -0500

    Add datafolder getter

--- api/src/main/java/us/crazycrew/crazycrates/api/ICrazyCrates.java
+++ api/src/main/java/us/crazycrew/crazycrates/api/ICrazyCrates.java
@@ -3,11 +3,15 @@ package us.crazycrew.crazycrates.api;
 import org.jetbrains.annotations.NotNull;
 import us.crazycrew.crazycrates.api.users.UserManager;
 
+import java.io.File;
+
 public interface ICrazyCrates {
 
     void enable();
 
     void disable();
+
+    File getDataFolder();
 
     @NotNull UserManager getUserManager();
 
