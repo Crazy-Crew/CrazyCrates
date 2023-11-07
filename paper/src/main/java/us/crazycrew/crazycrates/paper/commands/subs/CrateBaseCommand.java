e80a2a69a508bdb7c8bb26df115899a22fef1a92
Ryder Belserion <no-reply@ryderbelserion.com>
2023-11-07 00:54:56 -0500

    Track opened crates for mass open

--- paper/src/main/java/us/crazycrew/crazycrates/paper/commands/subs/CrateBaseCommand.java
+++ paper/src/main/java/us/crazycrew/crazycrates/paper/commands/subs/CrateBaseCommand.java
@@ -464,6 +464,7 @@ public class CrateBaseCommand extends BaseCommand {
             Prize prize = crate.pickPrize(player);
             this.plugin.getCrazyHandler().getPrizeManager().givePrize(player, prize, crate);
             this.plugin.getServer().getPluginManager().callEvent(new PlayerPrizeEvent(player, crate, crate.getName(), prize));
+            userManager.addOpenedCrate(player.getUniqueId(), crate.getName());
 
             if (prize.useFireworks()) MiscUtils.spawnFirework(((Player) sender).getLocation().clone().add(.5, 1, .5), null);
 
