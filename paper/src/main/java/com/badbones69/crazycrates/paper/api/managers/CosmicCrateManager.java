edb6b7d521492a44b8e2c9b4a2979d7bf7c1457e
Ryder Belserion <no-reply@ryderbelserion.com>
2023-11-21 22:58:27 -0500

    Move picked crates to cosmic manager

--- paper/src/main/java/com/badbones69/crazycrates/paper/api/managers/CosmicCrateManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/api/managers/CosmicCrateManager.java
@@ -3,20 +3,34 @@ package com.badbones69.crazycrates.paper.api.managers;
 import com.badbones69.crazycrates.paper.api.objects.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import us.crazycrew.crazycrates.paper.CrazyCrates;
+import us.crazycrew.crazycrates.paper.api.enums.PersistentKeys;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class CosmicCrateManager extends AbstractCrate {
-    
-    private final FileConfiguration file;
-    private final int totalPrizes;
+
     private final ItemBuilder mysteryCrate;
     private final ItemBuilder pickedCrate;
-    
+    private final FileConfiguration file;
+    private final int totalPrizes;
+
     // Picked items
+    private final Map<UUID, ArrayList<Integer>> pickedPrizes = new HashMap<>();
+    
     public CosmicCrateManager(CrazyCrates plugin, FileConfiguration file) {
         this.file = file;
+
         String path = "Crate.Crate-Type-Settings.";
+
         this.totalPrizes = file.getInt(path + "Total-Prize-Amount", 4);
+
         this.mysteryCrate = new ItemBuilder()
         .setMaterial(file.getString(path + "Mystery-Crate.Item", "CHEST"))
         .setName(file.getString(path + "Mystery-Crate.Name", "&f&l???"))
@@ -59,8 +73,20 @@ public class CosmicCrateManager extends AbstractCrate {
     public ItemBuilder getMysteryCrate() {
         return this.mysteryCrate;
     }
-    
+
     public ItemBuilder getPickedCrate() {
         return this.pickedCrate;
+    }
+
+    public void addPickedPrize(Player player, ArrayList<Integer> prizes) {
+        this.pickedPrizes.put(player.getUniqueId(), prizes);
+    }
+
+    public void removePickedPrize(Player player) {
+        this.pickedPrizes.remove(player.getUniqueId());
+    }
+
+    public Map<UUID, ArrayList<Integer>> getPickedPrizes() {
+        return Collections.unmodifiableMap(this.pickedPrizes);
     }
 }
