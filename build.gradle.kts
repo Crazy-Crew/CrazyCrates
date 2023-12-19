40549f4c75758b1563c8ad3fafeffadefbcd25f9
Ryder Belserion <no-reply@ryderbelserion.com>
2023-12-18 22:30:20 -0500

    push current shit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,26 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+import org.gradle.kotlin.dsl.support.uppercaseFirstChar
+
 plugins {
     `java-library`
+
+    alias(libs.plugins.modrinth)
+
+    alias(libs.plugins.hangar)
 }
 
+val mcVersion = rootProject.properties["minecraftVersion"] as String
+
+val version = rootProject.version as String
+val isRelease: Boolean = version.contains("-")
+val type = if (isRelease) "Release" else "Beta"
+
+val component: SoftwareComponent = components["java"]
+
 tasks {
+    val jarsDir = File("$rootDir/jars")
+
     assemble {
-        val jarsDir = File("$rootDir/jars")
         if (jarsDir.exists()) jarsDir.delete()
 
         jarsDir.mkdirs()
@@ -13,14 +29,66 @@ tasks {
             dependsOn(":${project.name}:build")
 
             doLast {
-                if (project.name == "common" || project.name == "api") return@doLast
-
-                copy {
-                    from(project.layout.buildDirectory.file("libs/${rootProject.name}-${rootProject.version}.jar"))
-                    into(jarsDir)
+                runCatching {
+                    copy {
+                        from(project.layout.buildDirectory.file("libs/${rootProject.name}-${project.name.uppercaseFirstChar()}-${version}.jar"))
+                        into(jarsDir)
+                    }
+                }.onSuccess {
+                    // Delete to save space on jenkins.
+                    delete(project.layout.buildDirectory.get())
+                    delete(rootProject.layout.buildDirectory.get())
+                }.onFailure {
+                    println("Failed to copy file out of build folder into jars directory: Likely does not exist.")
                 }
             }
         }
+    }
+
+    // Publish to hangar.papermc.io.
+    hangarPublish {
+        publications.register("plugin") {
+            version.set("$rootProject.version")
+
+            id.set(rootProject.name)
+
+            channel.set(type)
+
+            changelog.set(rootProject.file("CHANGELOG.md").readText())
+
+            apiKey.set(System.getenv("hangar_key"))
+
+            platforms {
+                register(Platforms.PAPER) {
+                    jar.set(file("$jarsDir/${rootProject.name}-Paper-${rootProject.version}.jar"))
+
+                    platformVersions.set(listOf(mcVersion))
+                }
+            }
+        }
+    }
+
+    // Publish to modrinth.
+    modrinth {
+        autoAddDependsOn.set(false)
+
+        token.set(System.getenv("modrinth_token"))
+
+        projectId.set("crazyrunes")
+
+        versionName.set("${rootProject.name} ${rootProject.version}")
+
+        versionNumber.set("${rootProject.version}")
+
+        versionType.set(type.lowercase())
+
+        uploadFile.set(file("$jarsDir/${rootProject.name}-Paper-${rootProject.version}.jar"))
+
+        gameVersions.add(mcVersion)
+
+        changelog.set(rootProject.file("CHANGELOG.md").readText())
+
+        loaders.addAll("paper", "purpur")
     }
 }
 
--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,18 +1,14 @@
-import io.papermc.hangarpublishplugin.model.Platforms
+import org.gradle.kotlin.dsl.support.uppercaseFirstChar
 
 plugins {
     alias(libs.plugins.paperweight)
     alias(libs.plugins.shadowjar)
 
-    alias(libs.plugins.modrinth)
-
     alias(libs.plugins.runpaper)
-
-    alias(libs.plugins.hangar)
 }
 
 base {
-    archivesName.set(rootProject.name)
+    archivesName.set("${rootProject.name}-${project.name.uppercaseFirstChar()}")
 }
 
 val mcVersion = rootProject.properties["minecraftVersion"] as String
@@ -43,69 +39,7 @@ dependencies {
     paperweightDevelopmentBundle("io.papermc.paper:dev-bundle:$mcVersion-R0.1-SNAPSHOT")
 }
 
-val isBeta: Boolean get() = rootProject.extra["isBeta"]?.toString()?.toBoolean() ?: false
-val type = if (isBeta) "Beta" else "Release"
-
-val description = """
-## Fixes:
- * Fix an NPE with item builder
-
-## Other:
- * [Feature Requests](https://github.com/Crazy-Crew/${rootProject.name}/issues)
- * [Bug Reports](https://github.com/Crazy-Crew/${rootProject.name}/issues)
-"""
-
-val component: SoftwareComponent = components["java"]
-
 tasks {
-    val file = project.layout.buildDirectory.file("libs/${rootProject.name}-${rootProject.version}.jar").get()
-
-    // Publish to hangar.papermc.io.
-    hangarPublish {
-        publications.register("plugin") {
-            version.set("$rootProject.version")
-
-            id.set(rootProject.name)
-
-            channel.set(type)
-
-            changelog.set(description)
-
-            apiKey.set(System.getenv("hangar_key"))
-
-            platforms {
-                register(Platforms.PAPER) {
-                    jar.set(file)
-
-                    platformVersions.set(listOf(mcVersion))
-                }
-            }
-        }
-    }
-
-    // Publish to modrinth.
-    modrinth {
-        autoAddDependsOn.set(false)
-
-        token.set(System.getenv("modrinth_token"))
-
-        projectId.set(rootProject.name.lowercase())
-
-        versionName.set("${rootProject.name} ${rootProject.version}")
-
-        versionNumber.set("${rootProject.version}")
-
-        versionType.set(type.lowercase())
-
-        uploadFile.set(file)
-
-        gameVersions.add(mcVersion)
-
-        changelog.set(description)
-
-        loaders.addAll("paper", "purpur")
-    }
-
     // Runs a test server.
     runServer {
         jvmArgs("-Dnet.kyori.ansi.colorLevel=truecolor")
