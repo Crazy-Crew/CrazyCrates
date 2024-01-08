3c016d10a1f6533604385a675cec423a381dfde2
Ryder Belserion <no-reply@ryderbelserion.com>
2024-01-07 19:34:33 -0500

    Update MiscListener.java

--- paper/src/main/java/com/badbones69/crazycrates/listeners/MiscListener.java
+++ paper/src/main/java/com/badbones69/crazycrates/listeners/MiscListener.java
@@ -51,7 +51,8 @@ public class MiscListener implements Listener {
         this.inventoryManager.removeCrateViewer(player);
         this.inventoryManager.removePageViewer(player);
 
-       if (crate != null {         this.crateManager.endQuickCrate(player, player.getLocation(), crate, false);
+       if (crate != null {         
+this.crateManager.endQuickCrate(player, player.getLocation(), crate, false);
        }
 
         // End just in case.
