c11128701325a817eb27892e77860a700029c4d1
Ryder Belserion <no-reply@ryderbelserion.com>
2024-02-05 17:10:06 -0500

    style: indentation

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -31,10 +31,10 @@ dependencies {
 tasks {
     shadowJar {
         listOf(
-                "com.ryderbelserion.cluster.paper",
-                "de.tr7zw.changeme.nbtapi",
-                "dev.triumphteam.cmd",
-                "org.bstats"
+            "com.ryderbelserion.cluster.paper",
+            "de.tr7zw.changeme.nbtapi",
+            "dev.triumphteam.cmd",
+            "org.bstats"
         ).forEach {
             relocate(it, "libs.$it")
         }
@@ -42,13 +42,13 @@ tasks {
 
     processResources {
         val properties = hashMapOf(
-                "name" to rootProject.name,
-                "version" to project.version,
-                "group" to rootProject.group,
-                "description" to rootProject.description,
-                "apiVersion" to rootProject.properties["apiVersion"],
-                "authors" to rootProject.properties["authors"],
-                "website" to rootProject.properties["website"]
+            "name" to rootProject.name,
+            "version" to project.version,
+            "group" to rootProject.group,
+            "description" to rootProject.description,
+            "apiVersion" to rootProject.properties["apiVersion"],
+            "authors" to rootProject.properties["authors"],
+            "website" to rootProject.properties["website"]
         )
 
         inputs.properties(properties)
