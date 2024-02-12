cf3d6fcb279d88a3c1cff1cd2841b5bb8fa2bf85
Ryder Belserion <no-reply@ryderbelserion.com>
2024-02-12 01:55:20 -0500

    How did it use itself?

--- paper/src/main/java/com/badbones69/crazycrates/api/PrizeManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/api/PrizeManager.java
@@ -152,7 +152,7 @@ public class PrizeManager {
      */
     public static void givePrize(Player player, Crate crate, Prize prize) {
         if (prize != null) {
-            givePrize(player, crate, prize);
+            givePrize(player, prize, crate);
 
             if (prize.useFireworks()) MiscUtils.spawnFirework(player.getLocation().add(0, 1, 0), null);
 
