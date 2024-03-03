6260fd7b162b53bf91adf27114ab093525b55f02
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-03 11:16:58 -0500

    Update jars folder handling

--- buildSrc/src/main/kotlin/fabric-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/fabric-plugin.gradle.kts
@@ -6,8 +6,8 @@ base {
     archivesName.set(rootProject.name)
 }
 
-val mcVersion = providers.gradleProperty("mcVersion")
-val fabricVersion = providers.gradleProperty("version")
+val mcVersion = providers.gradleProperty("mcVersion").get()
+val fabricVersion = providers.gradleProperty("version").get()
 
 project.version = if (System.getenv("BUILD_NUMBER") != null) "$fabricVersion-${System.getenv("BUILD_NUMBER")}" else fabricVersion
 
