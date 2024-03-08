219c2a28be4143c57460b6d985ffada481e08fc7
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-07 19:53:25 -0500

    Add import

--- paper/src/main/java/com/badbones69/crazycrates/tasks/BukkitUserManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/tasks/BukkitUserManager.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 
