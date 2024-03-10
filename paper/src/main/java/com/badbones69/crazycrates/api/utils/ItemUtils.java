90c0310e0af8b802b29353d8e46553136f47bbf9
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-10 19:36:23 -0400

    Allow use of old keys if the item meta contains the old identifier

--- paper/src/main/java/com/badbones69/crazycrates/api/utils/ItemUtils.java
+++ paper/src/main/java/com/badbones69/crazycrates/api/utils/ItemUtils.java
@@ -1,8 +1,10 @@
 package com.badbones69.crazycrates.api.utils;
 
 import com.badbones69.crazycrates.api.enums.PersistentKeys;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
 public class ItemUtils {
@@ -18,7 +20,21 @@ public class ItemUtils {
     }
 
     public static boolean isKey(ItemStack itemStack) {
-        return itemStack.getItemMeta().getPersistentDataContainer().has(PersistentKeys.crate_key.getNamespacedKey());
+        if (itemStack == null) return false;
+
+        if (itemStack.getType() == Material.AIR) return false;
+
+        if (!itemStack.hasItemMeta()) return false;
+
+        ItemMeta itemMeta = itemStack.getItemMeta();
+
+        boolean asString = itemMeta.getAsString().contains("CrazyCrates-Crate");
+
+        if (asString) {
+            return true;
+        }
+
+        return itemMeta.getPersistentDataContainer().has(PersistentKeys.crate_key.getNamespacedKey());
     }
 
     public static String getKey(ItemStack itemStack) {
