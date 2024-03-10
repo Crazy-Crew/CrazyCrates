5c4e6c3c066fbdae994edffab7ce27bea17207c2
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-10 19:37:09 -0400

    Re-work crate on the go

--- paper/src/main/java/com/badbones69/crazycrates/listeners/crates/MobileCrateListener.java
+++ paper/src/main/java/com/badbones69/crazycrates/listeners/crates/MobileCrateListener.java
@@ -2,6 +2,7 @@ package com.badbones69.crazycrates.listeners.crates;
 
 import com.badbones69.crazycrates.CrazyCrates;
 import com.badbones69.crazycrates.api.PrizeManager;
+import com.badbones69.crazycrates.api.enums.PersistentKeys;
 import com.badbones69.crazycrates.tasks.crates.CrateManager;
 import com.badbones69.crazycrates.api.events.PlayerPrizeEvent;
 import com.badbones69.crazycrates.api.objects.Crate;
@@ -13,6 +14,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import us.crazycrew.crazycrates.api.enums.types.CrateType;
 import com.badbones69.crazycrates.api.utils.ItemUtils;
@@ -36,23 +40,34 @@ public class MobileCrateListener implements Listener {
             if (item.getType() == Material.AIR) return;
 
             if (!item.hasItemMeta()) return;
-                if (crate.getCrateType() == CrateType.crate_on_the_go && this.crateManager.isKeyFromCrate(item, crate)) {
-                    event.setCancelled(true);
 
-                    this.crateManager.addPlayerToOpeningList(player, crate);
+            ItemMeta itemMeta = item.getItemMeta();
 
-                    ItemUtils.removeItem(item, player);
+            PersistentDataContainer container = itemMeta.getPersistentDataContainer();
 
-                    Prize prize = crate.pickPrize(player);
+            Crate crate = this.crateManager.getCrateFromName(container.get(PersistentKeys.crate_key.getNamespacedKey(), PersistentDataType.STRING));
 
-                    PrizeManager.givePrize(player, prize, crate);
-                    this.plugin.getServer().getPluginManager().callEvent(new PlayerPrizeEvent(player, crate, this.crateManager.getOpeningCrate(player).getName(), prize));
+            if (crate == null) return;
 
-                    if (prize.useFireworks()) MiscUtils.spawnFirework(player.getLocation().add(0, 1, 0), null);
+            if (crate.getCrateType() != CrateType.crate_on_the_go) return;
 
-                    this.crateManager.removePlayerFromOpeningList(player);
-                }
-            }
+            if (!this.crateManager.isKeyFromCrate(item, crate)) return;
+
+            event.setCancelled(true);
+
+            this.crateManager.addPlayerToOpeningList(player, crate);
+
+            ItemUtils.removeItem(item, player);
+
+            Prize prize = crate.pickPrize(player);
+
+            PrizeManager.givePrize(player, prize, crate);
+
+            this.plugin.getServer().getPluginManager().callEvent(new PlayerPrizeEvent(player, crate, this.crateManager.getOpeningCrate(player).getName(), prize));
+
+            if (prize.useFireworks()) MiscUtils.spawnFirework(player.getLocation().add(0, 1, 0), null);
+
+            this.crateManager.removePlayerFromOpeningList(player);
         }
     }
 }
