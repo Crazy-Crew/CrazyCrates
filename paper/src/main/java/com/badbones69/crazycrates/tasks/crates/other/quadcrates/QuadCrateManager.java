4e6fd7cb7e9d5ce87bbee1198e97bc1fedb19707
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-25 15:37:45 -0400

    Remove duplicate failed to take

--- paper/src/main/java/com/badbones69/crazycrates/tasks/crates/other/quadcrates/QuadCrateManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/tasks/crates/other/quadcrates/QuadCrateManager.java
@@ -177,8 +177,6 @@ public class QuadCrateManager {
         }
 
         if (!this.plugin.getUserManager().takeKeys(1, this.player.getUniqueId(), this.crate.getName(), this.keyType, this.checkHand)) {
-            MiscUtils.failedToTakeKey(this.player, this.crate.getName());
-
             this.crateManager.removePlayerFromOpeningList(this.player);
 
             crateSessions.remove(this.instance);
