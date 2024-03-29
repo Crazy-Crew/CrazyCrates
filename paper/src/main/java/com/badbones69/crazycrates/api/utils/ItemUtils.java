3b78a7bdcb9eec4cf42ffff7aaa113a0e9467c99
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-29 13:32:19 -0400

    Rename blank key variable

--- paper/src/main/java/com/badbones69/crazycrates/api/utils/ItemUtils.java
+++ paper/src/main/java/com/badbones69/crazycrates/api/utils/ItemUtils.java
@@ -32,10 +32,10 @@ public class ItemUtils {
 
     public static boolean isSimilar(ItemStack itemStack, Crate crate) {
         if (ConfigManager.getConfig().getProperty(ConfigKeys.use_old_key_checks)) {
-            return itemStack.isSimilar(crate.getKeyNoNBT())
-                    || itemStack.isSimilar(crate.getKeyNoNBT())
-                    || isSimilarCustom(crate.getKeyNoNBT(), itemStack)
-                    || crateManager.isKeyFromCrate(itemStack, crate);
+            boolean isSimilar = itemStack.isSimilar(crate.getEmptyKey());
+            boolean isCustom = isSimilarCustom(crate.getEmptyKey(), itemStack);
+
+            return isSimilar || isCustom || isKey;
         }
 
         return crateManager.isKeyFromCrate(itemStack, crate);
