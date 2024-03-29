bd5ccd0e2b60853b1a2c8c80125b03559f294189
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-29 13:42:55 -0400

    Revert convert changes

--- paper/src/main/java/com/badbones69/crazycrates/api/objects/Crate.java
+++ paper/src/main/java/com/badbones69/crazycrates/api/objects/Crate.java
@@ -89,7 +89,7 @@ public class Crate {
      * @param file The crate file.
      */
     public Crate(String name, String previewName, CrateType crateType, ItemStack key, String keyName, List<Prize> prizes, FileConfiguration file, int newPlayerKeys, List<Tier> tiers, int maxMassOpen, int requiredKeys, List<String> prizeMessage, List<String> prizeCommands, CrateHologram hologram) {
-        this.emptyKey = ItemBuilder.convertItemStack(key, false);
+        this.emptyKey = ItemBuilder.convertItemStack(key);
         this.keyBuilder = ItemBuilder.convertItemStack(key).setCrateName(name);
         this.keyName = keyName;
 
