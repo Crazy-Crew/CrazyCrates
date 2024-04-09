8319fcf96f1875f761aaabdb25395a236100a204
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-08 23:35:50 -0400

    remove extra glass

--- paper/src/main/java/com/badbones69/crazycrates/api/utils/MiscUtils.java
+++ paper/src/main/java/com/badbones69/crazycrates/api/utils/MiscUtils.java
@@ -286,21 +286,6 @@ public class MiscUtils {
                 Material.GRAY_STAINED_GLASS_PANE,
                 Material.LIME_STAINED_GLASS_PANE,
                 Material.PINK_STAINED_GLASS_PANE,
-                Material.RED_STAINED_GLASS_PANE,
-
-                Material.LIGHT_BLUE_STAINED_GLASS_PANE,
-                Material.MAGENTA_STAINED_GLASS_PANE,
-                Material.YELLOW_STAINED_GLASS_PANE,
-                Material.PURPLE_STAINED_GLASS_PANE,
-                Material.ORANGE_STAINED_GLASS_PANE,
-                Material.GREEN_STAINED_GLASS_PANE,
-                Material.BROWN_STAINED_GLASS_PANE,
-                Material.BLACK_STAINED_GLASS_PANE,
-                Material.BLUE_STAINED_GLASS_PANE,
-                Material.CYAN_STAINED_GLASS_PANE,
-                Material.GRAY_STAINED_GLASS_PANE,
-                Material.LIME_STAINED_GLASS_PANE,
-                Material.PINK_STAINED_GLASS_PANE,
                 Material.RED_STAINED_GLASS_PANE
         );
 
