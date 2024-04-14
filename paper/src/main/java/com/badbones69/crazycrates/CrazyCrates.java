d872a52dbca5fabe12013d4f549d2f184f4d656d
Euphyllia <bierque.euphyllia@gmail.com>
2024-04-14 18:19:35 +0200

    Restore
    Signed-off-by: Euphyllia <bierque.euphyllia@gmail.com>

--- paper/src/main/java/com/badbones69/crazycrates/CrazyCrates.java
+++ paper/src/main/java/com/badbones69/crazycrates/CrazyCrates.java
@@ -4,7 +4,6 @@ import com.badbones69.crazycrates.api.builders.types.CrateAdminMenu;
 import com.badbones69.crazycrates.api.builders.types.CrateMainMenu;
 import com.badbones69.crazycrates.api.builders.types.CratePreviewMenu;
 import com.badbones69.crazycrates.api.builders.types.CrateTierMenu;
-import com.badbones69.crazycrates.api.utils.FileUtils;
 import com.badbones69.crazycrates.api.utils.MiscUtils;
 import com.badbones69.crazycrates.api.utils.MsgUtils;
 import com.badbones69.crazycrates.commands.CommandManager;
@@ -17,51 +16,32 @@ import com.badbones69.crazycrates.listeners.crates.MobileCrateListener;
 import com.badbones69.crazycrates.listeners.crates.QuadCrateListener;
 import com.badbones69.crazycrates.listeners.crates.WarCrateListener;
 import com.badbones69.crazycrates.listeners.other.EntityDamageListener;
-import com.badbones69.crazycrates.platform.PaperServer;
-import com.badbones69.crazycrates.support.PluginSupport;
 import com.badbones69.crazycrates.support.holograms.HologramManager;
 import com.badbones69.crazycrates.support.metrics.MetricsManager;
 import com.badbones69.crazycrates.support.placeholders.PlaceholderAPISupport;
 import com.badbones69.crazycrates.tasks.BukkitUserManager;
 import com.badbones69.crazycrates.tasks.InventoryManager;
-import com.badbones69.crazycrates.tasks.MigrationManager;
 import com.badbones69.crazycrates.tasks.crates.CrateManager;
-
-import fr.euphyllia.energie.Energie;
-import fr.euphyllia.energie.model.Scheduler;
-import com.ryderbelserion.cluster.ClusterFactory;
-import net.minecraft.server.dedicated.DedicatedServer;
+import com.ryderbelserion.vital.VitalPaper;
+import com.ryderbelserion.vital.enums.Support;
+import com.ryderbelserion.vital.files.FileManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.badbones69.crazycrates.api.FileManager;
-import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
-import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
-import us.crazycrew.crazycrates.CrazyCrates;
+import us.crazycrew.crazycrates.platform.Server;
 import us.crazycrew.crazycrates.platform.config.ConfigManager;
 import us.crazycrew.crazycrates.platform.config.impl.ConfigKeys;
 import java.util.List;
 import java.util.Timer;
-
 import static com.badbones69.crazycrates.api.utils.MiscUtils.isLogging;
 import static com.badbones69.crazycrates.api.utils.MiscUtils.registerPermissions;
 
-public class CrazyCratesPaper extends JavaPlugin {
+public class CrazyCrates extends JavaPlugin {
 
-    private CrazyCrates instance;
-
-    @NotNull
-    public static CrazyCratesPaper get() {
-        return JavaPlugin.getPlugin(CrazyCratesPaper.class);
-    }
-
-    @NotNull
-    private final BukkitCommandManager<CommandSender> commandManager = BukkitCommandManager.create(this);
+    private Server instance;
 
     private final Timer timer;
 
-    private @NotNull Scheduler scheduler;
-
-    public CrazyCratesPaper() {
+    public CrazyCrates() {
         // Create timer object.
         this.timer = new Timer();
     }
@@ -69,20 +49,20 @@ public class CrazyCratesPaper extends JavaPlugin {
     private InventoryManager inventoryManager;
     private BukkitUserManager userManager;
     private CrateManager crateManager;
-    private FileManager fileManager;
 
     private MetricsManager metrics;
 
     @Override
+    public void onLoad() {
+        this.instance = new Server(this);
+        this.instance.enable();
+    }
+
+    @Override
     public void onEnable() {
-        scheduler = new Energie(this).getMinecraftScheduler();
-        // Migrate as early as possible.
-        MigrationManager.migrate();
-
-        // Load the config files.
-        ConfigManager.load(getDataFolder());
-
-        int radius = DedicatedServer.getServer().getSpawnProtectionRadius();
+        new VitalPaper(this);
+      
+        int radius = getServer().getSpawnRadius();
 
         if (radius > 0) {
             if (isLogging()) {
@@ -90,7 +70,6 @@ public class CrazyCratesPaper extends JavaPlugin {
                         "The spawn protection is set to " + radius,
                         "Crates placed in the spawn protection will not function",
                         "correctly as spawn protection overrides everything",
-                        "",
                         "Change the value in server.properties to 0 then restart"
                 ).forEach(getLogger()::warning);
             }
@@ -99,38 +78,14 @@ public class CrazyCratesPaper extends JavaPlugin {
         // Register permissions that we need.
         registerPermissions();
 
-        // The file manager is depended on by the user manager.
-        this.fileManager = new FileManager();
-
-        // Register files.
-        this.fileManager.registerDefaultGenerateFiles("CrateExample.yml", "/crates", "/crates")
-                .registerDefaultGenerateFiles("QuadCrateExample.yml", "/crates", "/crates")
-                .registerDefaultGenerateFiles("CosmicCrateExample.yml", "/crates", "/crates")
-                .registerDefaultGenerateFiles("QuickCrateExample.yml", "/crates", "/crates")
-                .registerDefaultGenerateFiles("WarCrateExample.yml", "/crates", "/crates")
-                .registerDefaultGenerateFiles("CasinoExample.yml", "/crates", "/crates")
-                .registerDefaultGenerateFiles("classic.nbt", "/schematics", "/schematics")
-                .registerDefaultGenerateFiles("nether.nbt", "/schematics", "/schematics")
-                .registerDefaultGenerateFiles("outdoors.nbt", "/schematics", "/schematics")
-                .registerDefaultGenerateFiles("sea.nbt", "/schematics", "/schematics")
-                .registerDefaultGenerateFiles("soul.nbt", "/schematics", "/schematics")
-                .registerDefaultGenerateFiles("wooden.nbt", "/schematics", "/schematics")
-                .registerCustomFilesFolder("/crates")
-                .registerCustomFilesFolder("/schematics")
-                .setup();
-
         this.inventoryManager = new InventoryManager();
         this.crateManager = new CrateManager();
         this.userManager = new BukkitUserManager();
 
-        // Init api
-        this.instance = new CrazyCrates(new PaperServer());
+        this.instance.setUserManager(this.userManager);
 
         // Load holograms.
         this.crateManager.loadHolograms();
-
-        // Load example files.
-        FileUtils.loadFiles();
 
         // Load the buttons.
         this.inventoryManager.loadButtons();
@@ -139,8 +94,7 @@ public class CrazyCratesPaper extends JavaPlugin {
         this.crateManager.loadCrates();
 
         // Load commands.
-        CommandManager commandManager = new CommandManager();
-        commandManager.load();
+        CommandManager.load();
 
         this.metrics = new MetricsManager();
 
@@ -172,16 +126,16 @@ public class CrazyCratesPaper extends JavaPlugin {
             String prefix = ConfigManager.getConfig().getProperty(ConfigKeys.console_prefix);
 
             // Print dependency garbage
-            for (PluginSupport value : PluginSupport.values()) {
-                if (value.isPluginEnabled()) {
-                    getServer().getConsoleSender().sendMessage(MsgUtils.color(prefix + "&6&l" + value.name() + " &a&lFOUND"));
+            for (Support value : Support.values()) {
+                if (value.isEnabled()) {
+                    getServer().getConsoleSender().sendMessage(MsgUtils.color(prefix + "&6&l" + value.getName() + " &a&lFOUND"));
                 } else {
-                    getServer().getConsoleSender().sendMessage(MsgUtils.color(prefix + "&6&l" + value.name() + " &c&lNOT FOUND"));
+                    getServer().getConsoleSender().sendMessage(MsgUtils.color(prefix + "&6&l" + value.getName() + " &c&lNOT FOUND"));
                 }
             }
         }
 
-        if (PluginSupport.PLACEHOLDERAPI.isPluginEnabled()) {
+        if (Support.placeholder_api.isEnabled()) {
             if (MiscUtils.isLogging()) getLogger().info("PlaceholderAPI support is enabled!");
 
             new PlaceholderAPISupport().register();
@@ -193,7 +147,9 @@ public class CrazyCratesPaper extends JavaPlugin {
     @Override
     public void onDisable() {
         // Cancel the timer task.
-        if (this.timer != null) this.timer.cancel();
+        if (this.timer != null) {
+            this.timer.cancel();
+        }
 
         // Clean up any mess we may have left behind.
         if (this.crateManager != null) {
@@ -201,52 +157,41 @@ public class CrazyCratesPaper extends JavaPlugin {
 
             HologramManager holograms = this.crateManager.getHolograms();
 
-            if (holograms != null && !holograms.isMapEmpty()) {
+            if (holograms != null && !holograms.isEmpty()) {
                 holograms.removeAllHolograms();
             }
         }
 
-        // Disable api
-        if (this.instance != null) this.instance.disable();
+        if (this.instance != null) {
+            this.instance.disable();
+        }
     }
 
-    @NotNull
-    public BukkitCommandManager<CommandSender> getCommandManager() {
-        return this.commandManager;
-    }
-
-    @NotNull
-    public InventoryManager getInventoryManager() {
+    public @NotNull InventoryManager getInventoryManager() {
         return this.inventoryManager;
     }
 
-    @NotNull
-    public BukkitUserManager getUserManager() {
+    public @NotNull BukkitUserManager getUserManager() {
         return this.userManager;
     }
 
-    @NotNull
-    public CrateManager getCrateManager() {
+    public @NotNull CrateManager getCrateManager() {
         return this.crateManager;
     }
 
-    @NotNull
-    public FileManager getFileManager() {
-        return this.fileManager;
+    public @NotNull FileManager getFileManager() {
+        return this.instance.getFileManager();
     }
 
-    @NotNull
-    public MetricsManager getMetrics() {
+    public @NotNull MetricsManager getMetrics() {
         return this.metrics;
     }
 
-    @NotNull
-    public Timer getTimer() {
-        return this.timer;
+    public @NotNull Server getInstance() {
+        return this.instance;
     }
 
-    @NotNull
-    public Scheduler getScheduler() {
-        return this.scheduler;
+    public @NotNull Timer getTimer() {
+        return this.timer;
     }
 }
