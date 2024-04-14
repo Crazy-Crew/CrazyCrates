fb2b64e5a6cf0d20139cbfe90201bc389d586330
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-14 00:53:08 -0400

    Add more git methods

--- buildSrc/src/main/kotlin/tools.kt
+++ buildSrc/src/main/kotlin/tools.kt
@@ -5,6 +5,16 @@ fun Project.latestCommitHistory(start: String, end: String): List<String> {
     return runGitCommand(listOf("log", "$start..$end", "--format=format:%h %s")).split("\n")
 }
 
+fun formatLog(commit: String, project: String): String {
+    val hash = commit.take(7)
+    val message = commit.substring(8) // Get message after commit hash + space between
+    return "[$hash](https://github.com/Crazy-Crew/$project/commit/$hash) $message<br>"
+}
+
+fun Project.latestCommitsHistory(): List<String> {
+    return runGitCommand(listOf("log", "--format=format:%h %s")).split("\n")
+}
+
 fun Project.latestCommitHash(): String {
     return runGitCommand(listOf("rev-parse", "--short", "HEAD"))
 }
