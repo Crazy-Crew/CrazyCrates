fdcc74acd80c213797d98581c6bce16989a064a5
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-14 10:13:01 -0400

    Don't create hologram if crate type is menu

--- paper/src/main/java/com/badbones69/crazycrates/support/holograms/types/CMIHologramsSupport.java
+++ paper/src/main/java/com/badbones69/crazycrates/support/holograms/types/CMIHologramsSupport.java
@@ -8,6 +8,7 @@ import us.crazycrew.crazycrates.api.crates.CrateHologram;
 import net.Zrips.CMILib.Container.CMILocation;
 import com.badbones69.crazycrates.api.objects.Crate;
 import com.badbones69.crazycrates.support.holograms.HologramManager;
+import us.crazycrew.crazycrates.api.enums.types.CrateType;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +18,8 @@ public class CMIHologramsSupport extends HologramManager {
 
     @Override
     public void createHologram(Location location, Crate crate) {
+        if (crate.getCrateType() == CrateType.menu) return;
+
         CrateHologram crateHologram = crate.getHologram();
 
         if (!crateHologram.isEnabled()) return;
