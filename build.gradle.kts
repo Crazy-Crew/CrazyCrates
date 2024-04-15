7393d0e8759c3b9aa5b24459ee723c70fe0301a3
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-15 01:21:23 -0400

    Move version assignment to paper-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     `root-plugin`
 }
 
-rootProject.version = if (System.getenv("BUILD_NUMBER") != null) "2.1-${System.getenv("BUILD_NUMBER")}" else "2.1"
 
 tasks {
     assemble {
--- buildSrc/src/main/kotlin/paper-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/paper-plugin.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("root-plugin")
 }
 
+project.version = if (System.getenv("BUILD_NUMBER") != null) "${rootProject.version}-${System.getenv("BUILD_NUMBER")}" else rootProject.version
+
 dependencies {
     paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
 }
