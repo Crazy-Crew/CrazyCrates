08a910247b828b59b123d06a5425c141d21ab2eb
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-30 00:02:47 -0400

    Add tier option to additem

--- paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/crates/CommandAddItem.java
+++ paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/crates/CommandAddItem.java
@@ -4,6 +4,7 @@ import com.badbones69.crazycrates.api.objects.Crate;
 import com.badbones69.crazycrates.commands.crates.types.BaseCommand;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.Command;
+import dev.triumphteam.cmd.core.annotations.Optional;
 import dev.triumphteam.cmd.core.annotations.Suggestion;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -13,7 +14,7 @@ public class CommandAddItem extends BaseCommand {
 
     @Command("additem")
     @Permission(value = "crazycrates.additem", def = PermissionDefault.OP)
-    public void add(Player player, @Suggestion("crates") String crateName, @Suggestion("prizes") String prizeName, @Suggestion("numbers") int chance) {
+    public void add(Player player, @Suggestion("crates") String crateName, @Suggestion("prizes") String prizeName, @Suggestion("numbers") int chance, @Suggestion("tiers") @Optional String tier) {
         Crate crate = getCrate(player, crateName, false);
 
         if (crate == null) {
@@ -21,6 +22,12 @@ public class CommandAddItem extends BaseCommand {
         }
 
         ItemStack item = player.getInventory().getItemInMainHand();
+
+        if (tier != null) {
+            crate.addEditorItem(prizeName, item, tier, chance);
+
+            return;
+        }
 
         crate.addEditorItem(prizeName, item, chance);
     }
