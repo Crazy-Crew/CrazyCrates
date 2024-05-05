8fd7ff8642da5f69c306194add19f11d67c8ea3c
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-05 18:24:46 -0400

    remove red from the old itembuilder

--- paper/src/main/java/com/badbones69/crazycrates/api/builders/ItemBuilder.java
+++ paper/src/main/java/com/badbones69/crazycrates/api/builders/ItemBuilder.java
@@ -3,11 +3,9 @@ package com.badbones69.crazycrates.api.builders;
 import com.badbones69.crazycrates.CrazyCrates;
 import com.badbones69.crazycrates.api.enums.PersistentKeys;
 import com.badbones69.crazycrates.api.hooks.HeadDatabaseListener;
-import com.ryderbelserion.vital.items.AbstractItemHandler;
 import com.ryderbelserion.vital.util.DyeUtil;
 import com.ryderbelserion.vital.util.ItemUtil;
 import me.arcaniax.hdb.api.HeadDatabaseAPI;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.DyeColor;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
@@ -25,7 +23,7 @@ import java.util.logging.Level;
 import java.util.stream.Collectors;
 import static com.ryderbelserion.vital.util.ItemUtil.getEnchantment;
 
-public class ItemBuilder extends AbstractItemHandler {
+public class ItemBuilder {
 
     private String crateName;
 
@@ -34,8 +32,6 @@ public class ItemBuilder extends AbstractItemHandler {
     }
 
     public ItemBuilder(ItemBuilder itemBuilder) {
-        super(itemBuilder);
-
         this.crateName = itemBuilder.crateName;
     }
 
@@ -55,7 +51,7 @@ public class ItemBuilder extends AbstractItemHandler {
      * @return the name of the crate.
      */
     public String getCrateName() {
-        return getString(PersistentKeys.crate_key.getNamespacedKey());
+        return "";
     }
 
     /**
@@ -209,315 +205,184 @@ public class ItemBuilder extends AbstractItemHandler {
     }
 
     public String getPlainDisplayName() {
-        return PlainTextComponentSerializer.plainText().serialize(getDisplayName());
+        return "";
     }
 
     // Super methods
-    @Override
+    
     public ItemBuilder getCompoundTag(ItemStack item, Player player) {
-        super.getCompoundTag(item, player);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setMaterial(Material material) {
-        super.setMaterial(material);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setMaterial(String material) {
-        super.setMaterial(material);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setNamePlaceholders(Map<String, String> placeholders) {
-        super.setNamePlaceholders(placeholders);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addNamePlaceholder(String placeholder, String argument) {
-        super.addNamePlaceholder(placeholder, argument);
-
         return this;
     }
 
-    @Override
     public ItemBuilder removeNamePlaceholder(String placeholder) {
-        super.removeNamePlaceholder(placeholder);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setDisplayName(String displayName) {
-        super.setDisplayName(displayName);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setLorePlaceholders(Map<String, String> placeholders) {
-        super.setLorePlaceholders(placeholders);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addLorePlaceholder(String placeholder, String argument) {
-        super.addLorePlaceholder(placeholder, argument);
-
         return this;
     }
 
-    @Override
     public ItemBuilder removeLorePlaceholder(String placeholder) {
-        super.removeLorePlaceholder(placeholder);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setDisplayLore(List<String> displayLore) {
-        super.setDisplayLore(displayLore);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setItemDamage(int itemDamage) {
-        super.setItemDamage(itemDamage);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setCompoundTag(String tag) {
-        super.setCompoundTag(tag);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addDisplayLore(String value) {
-        super.addDisplayLore(value);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setPatterns(Map<PatternType, DyeColor> types) {
-        super.setPatterns(types);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addPatterns(List<String> patterns) {
-        super.addPatterns(patterns);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addPattern(String pattern) {
-        super.addPattern(pattern);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addPattern(PatternType pattern, DyeColor color) {
-        super.addPattern(pattern, color);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setTrimMaterial(String material) {
-        super.setTrimMaterial(material);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setTrimPattern(String pattern) {
-        super.setTrimPattern(pattern);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setPotionAmplifier(int potionAmplifier) {
-        super.setPotionAmplifier(potionAmplifier);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setPotionDuration(int potionDuration) {
-        super.setPotionDuration(potionDuration);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setGlowing(boolean isGlowing) {
-        super.setGlowing(isGlowing);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setUnbreakable(boolean unbreakable) {
-        super.setUnbreakable(unbreakable);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setAmount(int amount) {
-        super.setAmount(amount);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setEffects(List<FireworkEffect.Builder> effects) {
-        super.setEffects(effects);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addEffect(FireworkEffect.Builder effect) {
-        super.addEffect(effect);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setFireworkPower(int power) {
-        super.setFireworkPower(power);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setEntityType(EntityType entityType) {
-        super.setEntityType(entityType);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addEnchantments(Map<String, Integer> enchantments, boolean ignoreLevelRestriction) {
-        super.addEnchantments(enchantments, ignoreLevelRestriction);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addEnchantment(String value, int level, boolean ignoreLevelRestriction) {
-        super.addEnchantment(value, level, ignoreLevelRestriction);
-
         return this;
     }
 
-    @Override
     public ItemBuilder removeEnchantment(String value) {
-        super.removeEnchantment(value);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setPlayer(String name) {
-        super.setPlayer(name);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setSkull(String skull, HeadDatabaseAPI api) {
-        super.setSkull(skull, api);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setTarget(Player target) {
-        super.setTarget(target);
-
         return this;
     }
 
-    @Override
     public ItemBuilder hideItemFlags(boolean hideItemFlags) {
-        super.hideItemFlags(hideItemFlags);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addItemFlags(List<String> flags) {
-        super.addItemFlags(flags);
-
         return this;
     }
 
-    @Override
     public ItemBuilder addItemFlag(ItemFlag itemFlag) {
-        super.addItemFlag(itemFlag);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setString(NamespacedKey key, String value) {
-        super.setString(key, value);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setDouble(NamespacedKey key, double value) {
-        super.setDouble(key, value);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setInteger(NamespacedKey key, int value) {
-        super.setInteger(key, value);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setBoolean(NamespacedKey key, boolean value) {
-        super.setBoolean(key, value);
-
         return this;
     }
 
-    @Override
     public ItemBuilder setList(NamespacedKey key, List<String> values) {
-        super.setList(key, values);
-
         return this;
     }
 
-    @Override
     public ItemBuilder removeKey(NamespacedKey key) {
-        super.removeKey(key);
-
         return this;
     }
 }
