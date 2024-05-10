8f11f90a8437d7abf96c6307fd39a7a8ac36fae8
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-09 21:35:24 -0400

    change vital api to 1.0

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper", "paper-api", "1.20.6-R0.1-SNAPSHOT")
 
-    api("com.ryderbelserion", "vital-paper", "1.20.6-snapshot")
+    api("com.ryderbelserion", "vital-paper", "1.0")
 
     api("ch.jalu", "configme", "1.4.1") {
         exclude("org.yaml")
