5b8aed0727f7f27d26eaddaa6003dbfaa14f4174
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-14 10:40:07 -0400

    only save to file if options to migrate were found

--- src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/crates/migrator/types/deprecation/DeprecatedCrateMigrator.java
+++ src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/crates/migrator/types/deprecation/DeprecatedCrateMigrator.java
@@ -70,6 +70,7 @@ public class DeprecatedCrateMigrator extends ICrateMigrator {
                     }
                 }
 
+                if (isSave) {
                     customFile.save();
                 }
 
