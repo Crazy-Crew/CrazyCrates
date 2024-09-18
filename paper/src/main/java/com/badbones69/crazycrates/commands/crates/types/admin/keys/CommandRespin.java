b4212e917936aa1ef0135142748c69be33808ea1
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-17 20:22:13 -0400

    add commands to accept/deny

--- paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/keys/CommandRespin.java
+++ paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/keys/CommandRespin.java
@@ -5,10 +5,12 @@ import com.badbones69.crazycrates.api.enums.Messages;
 import com.badbones69.crazycrates.api.objects.Crate;
 import com.badbones69.crazycrates.commands.crates.types.BaseCommand;
 import com.badbones69.crazycrates.tasks.BukkitUserManager;
+import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.Command;
 import dev.triumphteam.cmd.core.annotations.Suggestion;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionDefault;
 import org.jetbrains.annotations.NotNull;
 import us.crazycrew.crazycrates.api.enums.types.CrateType;
 
@@ -18,6 +20,72 @@ public class CommandRespin extends BaseCommand {
     @Permission(value = "crazycrates.respin.use", def = PermissionDefault.OP, description = "Allows the sender to access /crazycrates respin.")
     public void root(final CommandSender sender) {
         //todo() send help message
+    }
+
+    public class CommandRespinAccept {
+
+        protected @NotNull final CrazyCrates plugin = CrazyCrates.getPlugin();
+
+        protected @NotNull final BukkitUserManager userManager = this.plugin.getUserManager();
+
+        @Command("accept")
+        @Permission(value = "crazycrates.respin.accept", def = PermissionDefault.OP, description = "Allows the sender to accept the respin for another person.")
+        public void accept(final CommandSender sender, @Suggestion("players") final Player target, @Suggestion("crates") final String crateName, @Suggestion("prizes") final String prizeName) {
+            if (crateName == null || crateName.isEmpty() || crateName.isBlank()) {
+                Messages.cannot_be_empty.sendMessage(sender, "{value}", "crate name");
+
+                return;
+            }
+
+            final Crate crate = getCrate(sender, crateName, false);
+
+            if (crate == null || crate.getCrateType() == CrateType.menu) {
+                Messages.not_a_crate.sendMessage(sender, "{crate}", crateName);
+
+                return;
+            }
+
+            if (!this.userManager.hasRespinPrize(target.getUniqueId(), crateName)) {
+                //todo() they don't have a prize, so no accept.
+
+                return;
+            }
+
+            this.userManager.addRespinPrize(target.getUniqueId(), crate.getFileName(), prizeName);
+        }
+    }
+
+    public class CommandRespinDeny {
+
+        protected @NotNull final CrazyCrates plugin = CrazyCrates.getPlugin();
+
+        protected @NotNull final BukkitUserManager userManager = this.plugin.getUserManager();
+
+        @Command("deny")
+        @Permission(value = "crazycrates.respin.deny", def = PermissionDefault.OP, description = "Allows the sender to deny the respin for another person.")
+        public void deny(final CommandSender sender, @Suggestion("players") final Player target, @Suggestion("crates") final String crateName) {
+            if (crateName == null || crateName.isEmpty() || crateName.isBlank()) {
+                Messages.cannot_be_empty.sendMessage(sender, "{value}", "crate name");
+
+                return;
+            }
+
+            final Crate crate = getCrate(sender, crateName, false);
+
+            if (crate == null || crate.getCrateType() == CrateType.menu) {
+                Messages.not_a_crate.sendMessage(sender, "{crate}", crateName);
+
+                return;
+            }
+
+            if (!this.userManager.hasRespinPrize(target.getUniqueId(), crateName)) {
+                //todo() they don't have a prize, so no accept.
+
+                return;
+            }
+
+            this.userManager.removeRespinPrize(target.getUniqueId(), crate.getFileName());
+        }
     }
 
     public class CommandRespinAdd {
