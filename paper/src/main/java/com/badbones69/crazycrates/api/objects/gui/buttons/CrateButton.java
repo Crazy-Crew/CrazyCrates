198308705793f305febd73583d35fb56933616ec
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-19 20:41:17 -0400

    create a crate button class extending gui button
    useful for crate related buttons

---
+++ paper/src/main/java/com/badbones69/crazycrates/api/objects/gui/buttons/CrateButton.java
@@ -0,0 +1,77 @@
+package com.badbones69.crazycrates.api.objects.gui.buttons;
+
+import com.badbones69.crazycrates.api.PrizeManager;
+import com.badbones69.crazycrates.api.objects.Crate;
+import com.badbones69.crazycrates.api.objects.Prize;
+import com.badbones69.crazycrates.managers.BukkitUserManager;
+import com.badbones69.crazycrates.tasks.crates.CrateManager;
+import com.ryderbelserion.vital.paper.api.builders.gui.interfaces.GuiAction;
+import com.ryderbelserion.vital.paper.api.builders.gui.interfaces.GuiItem;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.jetbrains.annotations.NotNull;
+import us.crazycrew.crazycrates.api.enums.types.KeyType;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class CrateButton extends GuiButton {
+
+    private final BukkitUserManager userManager = this.plugin.getUserManager();
+    private final CrateManager crateManager = this.plugin.getCrateManager();
+
+    private final Crate crate;
+    private final Prize prize;
+
+    public CrateButton(final Crate crate, final Prize prize, final ConfigurationSection section) {
+        super(section, new HashMap<>() {{
+            put("%crate_pretty%", crate.getCrateName());
+            put("%crate_raw%", crate.getFileName());
+            put("%prize%", prize.getSectionName());
+        }});
+
+        this.crate = crate;
+        this.prize = prize;
+    }
+
+    @Override
+    public final @NotNull GuiItem getGuiItem() {
+        final GuiItem guiItem = super.getGuiItem();
+
+        final GuiAction<InventoryClickEvent> action = guiItem.getAction();
+
+        guiItem.setAction(event -> {
+            if (action != null) {
+                action.execute(event);
+            }
+
+            final Player player = (Player) event.getWhoClicked();
+            final UUID uuid = player.getUniqueId();
+
+            switch (getSection().getName()) {
+                case "accept" -> {
+                    this.userManager.addRespinCrate(uuid, this.crate.getFileName(), 1);
+                    this.crateManager.openCrate(player, this.crate, KeyType.free_key, player.getLocation(), true, false);
+                }
+
+                case "deny" -> {
+                    final Prize prize = this.crate.getPrize(this.userManager.getRespinPrize(uuid, this.crate.getFileName()));
+
+                    PrizeManager.givePrize(player, prize, this.crate);
+
+                    this.userManager.removeRespinPrize(uuid, this.crate.getFileName());
+                }
+            }
+        });
+
+        return guiItem;
+    }
+
+    public final Crate getCrate() {
+        return this.crate;
+    }
+
+    public final Prize getPrize() {
+        return this.prize;
+    }
+}
