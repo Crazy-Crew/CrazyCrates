bf97d6079dc0d645418097b4a744edd661f04390
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-27 19:33:20 -0400

    reoad vital config

--- paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/CommandReload.java
+++ paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/CommandReload.java
@@ -4,6 +4,7 @@ import com.badbones69.crazycrates.api.enums.Messages;
 import com.badbones69.crazycrates.utils.MiscUtils;
 import com.badbones69.crazycrates.commands.crates.types.BaseCommand;
 import com.badbones69.crazycrates.managers.config.impl.ConfigKeys;
+import com.ryderbelserion.vital.common.config.ConfigManager;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.Command;
 import org.bukkit.command.CommandSender;
@@ -14,6 +15,8 @@ public class CommandReload extends BaseCommand {
     @Command("reload")
     @Permission(value = "crazycrates.reload", def = PermissionDefault.OP)
     public void reload(CommandSender sender) {
+        ConfigManager.reload();
+
         MiscUtils.janitor();
 
         this.plugin.getInstance().reload();
