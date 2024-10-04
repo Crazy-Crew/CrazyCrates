0cc144c810f81769ad53696dd015558283e88426
Ryder Belserion <no-reply@ryderbelserion.com>
2024-10-04 01:22:39 -0400

    unnecessary method

--- common/src/main/java/com/ryderbelserion/crazycrates/common/plugin/logger/AbstractLogger.java
+++ common/src/main/java/com/ryderbelserion/crazycrates/common/plugin/logger/AbstractLogger.java
@@ -1,42 +1,38 @@
 package com.ryderbelserion.crazycrates.common.plugin.logger;
 
+import com.ryderbelserion.vital.common.util.AdvUtil;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class AbstractLogger implements PluginLogger {
 
     private final ComponentLogger logger;
 
-    private final MiniMessage miniMessage;
-
     public AbstractLogger(final ComponentLogger logger) {
         this.logger = logger;
-
-        this.miniMessage = MiniMessage.miniMessage();
     }
 
     @Override
     public void info(String message) {
-        this.logger.info(this.miniMessage.deserialize(message));
+        this.logger.info(AdvUtil.parse(message));
     }
 
     @Override
     public void warn(String message) {
-        this.logger.warn(this.miniMessage.deserialize(message));
+        this.logger.warn(AdvUtil.parse(message));
     }
 
     @Override
     public void warn(String message, Throwable throwable) {
-        this.logger.warn(this.miniMessage.deserialize(message), throwable);
+        this.logger.warn(AdvUtil.parse(message), throwable);
     }
 
     @Override
     public void error(String message) {
-        this.logger.error(this.miniMessage.deserialize(message));
+        this.logger.error(AdvUtil.parse(message));
     }
 
     @Override
     public void error(String message, Throwable throwable) {
-        this.logger.error(this.miniMessage.deserialize(message), throwable);
+        this.logger.error(AdvUtil.parse(message), throwable);
     }
 }
