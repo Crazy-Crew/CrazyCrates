a1e4200e8e8ad03778d29728e552f806ccb0b2e6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-10-05 13:14:07 -0400

    add prize not found message

--- common/src/main/java/com/badbones69/crazycrates/common/config/impl/messages/ErrorKeys.java
+++ common/src/main/java/com/badbones69/crazycrates/common/config/impl/messages/ErrorKeys.java
@@ -20,6 +20,9 @@ public class ErrorKeys implements SettingsHolder {
     @Comment("A list of available placeholders: {crate}")
     public static final Property<String> no_prizes_found = newProperty("errors.no-prizes-found", "{prefix}<red>This crate contains no prizes that you can win.");
 
+    @Comment("A list of available placeholders: {prize}")
+    public static final Property<String> prize_not_found = newProperty("errors.prize-not-found", "{prefix}<red>The prize named {prize} entered could not be found.");
+
     public static final Property<String> no_schematics_found = newProperty("errors.no-schematics-found", "{prefix}<red>No schematics were found. Please make sure files ending in .nbt exist in the schematics folder. If not delete the folder so they regenerate.");
 
     public static final Property<String> internal_error = newProperty("errors.internal-error", "{prefix}<red>An internal error has occurred. Please check the console for the full error.");
