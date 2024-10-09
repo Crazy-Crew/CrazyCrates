6a5f60c49cf6ae217939d65e5cb1c622ad521072
Ryder Belserion <no-reply@ryderbelserion.com>
2024-10-08 22:29:19 -0400

    remove duplicate javadocs

--- paper/src/main/java/com/badbones69/crazycrates/tasks/crates/CrateManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/tasks/crates/CrateManager.java
@@ -559,10 +559,6 @@ public class CrateManager {
      * @param location the location that may be needed for some crate types.
      * @param checkHand if it just checks the players hand or if it checks their inventory.
      * @param eventType {@link EventType}
-     * @param player the player that is having the crate opened for them
-     * @param crate the crate that is being used
-     * @param location the location that may be needed for some crate types
-     * @param checkHand if it just checks the players hand or if it checks their inventory
      */
     public void openCrate(@NotNull final Player player, @NotNull final Crate crate, @NotNull final KeyType keyType, @NotNull final Location location, final boolean virtualCrate, final boolean checkHand, final EventType eventType) {
         openCrate(player, crate, keyType, location, virtualCrate, checkHand, false, eventType);
