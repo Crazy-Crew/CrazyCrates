eea1939473553af018a0ef63293bbe689ae7a1ff
Ryder Belserion <no-reply@ryderbelserion.com>
2024-12-17 01:48:19 -0500

    copy files out of resources directory

--- spoon/src/main/java/com/badbones69/crazycrates/CrazyCrates.java
+++ spoon/src/main/java/com/badbones69/crazycrates/CrazyCrates.java
@@ -1,16 +1,27 @@
 package com.badbones69.crazycrates;
 
 import com.ryderbelserion.FusionApi;
+import com.ryderbelserion.api.enums.FileType;
+import com.ryderbelserion.paper.files.FileManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 public class CrazyCrates extends JavaPlugin {
 
     private final FusionApi api = FusionApi.get();
 
+    private FileManager fileManager;
+
     @Override
     public void onEnable() {
         this.api.enable(this);
+
+        this.fileManager = this.api.getFileManager();
+
+        this.fileManager.addFile("respin-gui.yml", "guis", false, FileType.YAML)
+                .addFile("crates.log", "logs", false, FileType.NONE)
+                .addFile("keys.log", "logs", false, FileType.NONE)
+                .addFolder("crates", FileType.YAML)
+                .addFolder("schematics", FileType.NONE);
     }
 
     @Override
@@ -18,7 +29,11 @@ public class CrazyCrates extends JavaPlugin {
         this.api.disable();
     }
 
-    public @NotNull FusionApi getApi() {
+    public FileManager getFileManager() {
+        return this.fileManager;
+    }
+
+    public FusionApi getApi() {
         return this.api;
     }
 }
