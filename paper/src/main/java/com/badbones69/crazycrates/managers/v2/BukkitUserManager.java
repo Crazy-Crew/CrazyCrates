6e101dca79a722627d30bc969ebf930e7b194c8d
Ryder Belserion <no-reply@ryderbelserion.com>
2024-12-21 16:50:45 -0500

    add physical key section

--- paper/src/main/java/com/badbones69/crazycrates/managers/v2/BukkitUserManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/managers/v2/BukkitUserManager.java
@@ -1,17 +1,37 @@
 package com.badbones69.crazycrates.managers.v2;
 
+import ch.jalu.configme.SettingsManager;
 import com.badbones69.crazycrates.CrazyCrates;
+import com.badbones69.crazycrates.api.enums.Messages;
+import com.badbones69.crazycrates.api.enums.other.keys.ItemKeys;
 import com.badbones69.crazycrates.api.exception.CratesException;
+import com.badbones69.crazycrates.api.objects.Crate;
+import com.badbones69.crazycrates.common.config.ConfigManager;
+import com.badbones69.crazycrates.common.config.impl.ConfigKeys;
 import com.badbones69.crazycrates.managers.data.DataManager;
 import com.badbones69.crazycrates.managers.data.interfaces.Connector;
+import com.badbones69.crazycrates.tasks.crates.CrateManager;
+import com.badbones69.crazycrates.utils.MiscUtils;
+import io.papermc.paper.persistence.PersistentDataContainerView;
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import us.crazycrew.crazycrates.api.enums.types.KeyType;
 import java.sql.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class BukkitUserManager {
 
     private final CrazyCrates plugin = CrazyCrates.getPlugin();
+
+    private final ComponentLogger logger = this.plugin.getComponentLogger();
 
     private final DataManager dataManager = this.plugin.getDataManager();
 
@@ -108,15 +128,40 @@ public class BukkitUserManager {
     }
 
     public void addKeys(final UUID uuid, final String crateName, final int amount, final KeyType keyType) {
-        //todo() fetch crate from cache.
-
         switch (keyType) {
             case physical_key -> {
                 final Player player = this.plugin.getServer().getPlayer(uuid);
 
                 if (player == null) return;
 
-                //todo() add keys to inventory
+                final Crate crate = this.crateManager.getCrateFromName(crateName);
+
+                if (!MiscUtils.isInventoryFull(player)) {
+                    MiscUtils.addItem(player, crate.getKey(amount, player));
+
+                    return;
+                }
+
+                final SettingsManager config = ConfigManager.getConfig();
+
+                if (config.getProperty(ConfigKeys.give_virtual_keys_when_inventory_full)) {
+                    addKeys(uuid, crateName, amount, KeyType.virtual_key);
+
+                    if (config.getProperty(ConfigKeys.notify_player_when_inventory_full)) {
+                        final Map<String, String> placeholders = new HashMap<>();
+
+                        placeholders.put("{amount}", String.valueOf(amount));
+                        placeholders.put("{player}", player.getName());
+                        placeholders.put("{keytype}", keyType.getFriendlyName());
+                        placeholders.put("{key}", crate.getKeyName());
+
+                        Messages.cannot_give_player_keys.sendMessage(player, placeholders);
+                    }
+
+                    return;
+                }
+
+                player.getWorld().dropItem(player.getLocation(), crate.getKey(amount, player));
             }
 
             case virtual_key -> {
@@ -139,8 +184,6 @@ public class BukkitUserManager {
     }
 
     public void setKeys(final UUID uuid, final String crateName, final int amount, final KeyType keyType) {
-        //todo() fetch crate from cache.
-
         switch (keyType) {
             case physical_key -> addKeys(uuid, crateName, amount, keyType);
 
@@ -161,5 +204,13 @@ public class BukkitUserManager {
                 }
             }
         }
+    }
+
+    private final CrateManager crateManager = this.plugin.getCrateManager();
+
+    public @Nullable Crate getCrate(@NotNull final String crateName) {
+        if (crateName.isBlank()) return null;
+
+        return this.crateManager.getCrateFromName(crateName);
     }
 }
