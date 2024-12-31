21db65772cfb13a07d8a92612b21ee38050c68b6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-12-30 23:32:35 -0500

    chore: bump dependencies
    Updated to 1.21.4
    Updated paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("crates.base")
 }
+
+val buildNumber: String? = System.getenv("BUILD_NUMBER")
+
+rootProject.version = if (buildNumber != null) "${libs.versions.minecraft.get()}-$buildNumber" else "5.0.0"
+
+subprojects.filter { it.name != "api" }.forEach {
+    it.project.version = rootProject.version
+}
