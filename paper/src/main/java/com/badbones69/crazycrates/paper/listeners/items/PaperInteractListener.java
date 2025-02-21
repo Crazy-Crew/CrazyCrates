0f8f6d5c8f357ed345994742f6c227ac0ed65b6c
Ryder Belserion <contact@ryderbelserion.com>
2025-02-21 12:12:57 -0500

    Add old left click to break crate back because yes

--- paper/src/main/java/com/badbones69/crazycrates/paper/listeners/items/PaperInteractListener.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/listeners/items/PaperInteractListener.java
@@ -1,8 +1,10 @@
 package com.badbones69.crazycrates.paper.listeners.items;
 
 import com.badbones69.crazycrates.paper.CrazyCrates;
+import com.badbones69.crazycrates.paper.api.enums.Messages;
 import com.badbones69.crazycrates.paper.api.events.CrateInteractEvent;
 import com.badbones69.crazycrates.paper.tasks.crates.CrateManager;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -33,13 +35,25 @@ public class PaperInteractListener implements Listener {
 
         final Action action = event.getAction();
 
-        if (!this.crateManager.hasEditorCrate(player)) return;
-
         final Location location = block.getLocation();
 
         switch (action) {
-            case RIGHT_CLICK_BLOCK -> this.crateManager.addCrateByLocation(player, location);
-            case LEFT_CLICK_BLOCK -> this.crateManager.removeCrateByLocation(player, location);
+            case RIGHT_CLICK_BLOCK -> {
+                if (!this.crateManager.hasEditorCrate(player)) return;
+
+                this.crateManager.addCrateByLocation(player, location);
+            }
+            case LEFT_CLICK_BLOCK -> {
+                if (player.getGameMode() == GameMode.CREATIVE && player.isSneaking() && player.hasPermission("crazycrates.admin")) {
+                    this.crateManager.removeCrateByLocation(player, location);
+
+                    return;
+                }
+
+                if (!this.crateManager.hasEditorCrate(player)) return;
+
+                this.crateManager.removeCrateByLocation(player, location);
+            }
         }
 
         event.setCancelled(true);
