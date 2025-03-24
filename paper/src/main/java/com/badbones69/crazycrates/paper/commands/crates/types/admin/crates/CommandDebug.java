d3bbc0240f19587f75686b03ec0289dc4975402c
Ryder Belserion <contact@ryderbelserion.com>
2025-03-24 13:50:05 -0400

    Update debug command

--- paper/src/main/java/com/badbones69/crazycrates/paper/commands/crates/types/admin/crates/CommandDebug.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/commands/crates/types/admin/crates/CommandDebug.java
@@ -3,6 +3,7 @@ package com.badbones69.crazycrates.paper.commands.crates.types.admin.crates;
 import com.badbones69.crazycrates.paper.api.PrizeManager;
 import com.badbones69.crazycrates.paper.api.enums.Messages;
 import com.badbones69.crazycrates.paper.api.objects.Crate;
+import com.badbones69.crazycrates.paper.api.objects.Prize;
 import com.badbones69.crazycrates.paper.commands.crates.types.BaseCommand;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.ArgName;
@@ -13,6 +14,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionDefault;
 import us.crazycrew.crazycrates.api.enums.types.CrateType;
+import java.util.List;
 
 public class CommandDebug extends BaseCommand {
 
@@ -33,12 +35,22 @@ public class CommandDebug extends BaseCommand {
             return;
         }
 
-        if (sender instanceof Player player && target == null) {
-            crate.getPrizes().forEach(prize -> PrizeManager.givePrize(player, crate, prize));
+        final List<Prize> prizes = crate.getPrizes();
+
+        final List<Prize> items = prizes.stream().filter(Prize::isItemsNotEmpty).toList();
+
+        if (prizes.isEmpty() || items.isEmpty()) {
+            Messages.no_prizes_found.sendMessage(sender, "{crate}", crateName);
 
             return;
         }
 
-        crate.getPrizes().forEach(prize -> PrizeManager.givePrize(target, crate, prize));
+        if (sender instanceof Player player && target == null) {
+            prizes.forEach(prize -> PrizeManager.givePrize(player, crate, prize));
+
+            return;
+        }
+
+        prizes.forEach(prize -> PrizeManager.givePrize(target, crate, prize));
     }
 }
