584d1133a139e8a8d6790188ba26cc80bab9fafb
Ryder Belserion <contact@ryderbelserion.com>
2025-05-20 00:36:13 -0400

    Update root script file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,126 @@
 plugins {
+    alias(libs.plugins.minotaur)
+    alias(libs.plugins.feather)
+
     `config-java`
 }
 
 rootProject.group = "com.badbones69.crazycrates"
 
-val buildNumber: String? = System.getenv("BUILD_NUMBER")
-val isPublishing: String? = System.getenv("IS_PUBLISHING")
+val git = feather.getGit()
 
-rootProject.version = if (buildNumber != null && isPublishing == null) "${libs.versions.minecraft.get()}-$buildNumber" else rootProject.properties["version"].toString()
+val commitHash: String? = git.getCurrentCommitHash().subSequence(0, 7).toString()
+val isSnapshot: Boolean = true
+val content: String = if (isSnapshot) "[$commitHash](https://github.com/Crazy-Crew/${rootProject.name}/commit/$commitHash) ${git.getCurrentCommit()}" else rootProject.file("changelog.md").readText(Charsets.UTF_8)
+
+rootProject.version = if (isSnapshot) "${libs.versions.minecraft.get()}-$commitHash" else libs.versions.crazycrates.get()
+rootProject.description = "Add crates to your server with 11 different crate types to choose from!"
+
+feather {
+    rootDirectory = rootProject.rootDir.toPath()
+
+    val data = git.getCurrentCommitAuthorData()
+
+    discord {
+        webhook {
+            group(rootProject.name.lowercase())
+            task("dev-build")
+
+            if (System.getenv("BUILD_WEBHOOK") != null) {
+                post(System.getenv("BUILD_WEBHOOK"))
+            }
+
+            username(data.author)
+
+            avatar(data.avatar)
+
+            embeds {
+                embed {
+                    color("#ffa347")
+
+                    title("A new dev version of ${rootProject.name} is ready!")
+
+                    fields {
+                        field(
+                            "Version ${rootProject.version}",
+                            "Click [here](https://modrinth.com/plugin/${rootProject.name.lowercase()}/version/${rootProject.version}) to download!"
+                        )
+
+                        field(
+                            ":bug: Report Bugs",
+                            "https://github.com/Crazy-Crew/${rootProject.name}/issues"
+                        )
+
+                        field(
+                            ":hammer: Changelog",
+                            content
+                        )
+                    }
+                }
+            }
+        }
+
+        webhook {
+            group(rootProject.name.lowercase())
+            task("release-build")
+
+            if (System.getenv("BUILD_WEBHOOK") != null) {
+                post(System.getenv("BUILD_WEBHOOK"))
+            }
+
+            username(data.author)
+
+            avatar(data.avatar)
+
+            content("<@&1372358375433834537>")
+
+            embeds {
+                embed {
+                    color("#1bd96a")
+
+                    title("A new release version of ${rootProject.name} is ready!")
+
+                    fields {
+                        field(
+                            "Version ${rootProject.version}",
+                            "Click [here](https://modrinth.com/plugin/${rootProject.name.lowercase()}/version/${rootProject.version}) to download!"
+                        )
+
+                        field(
+                            ":bug: Report Bugs",
+                            "https://github.com/Crazy-Crew/${rootProject.name}/issues"
+                        )
+
+                        field(
+                            ":hammer: Changelog",
+                            content
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+modrinth {
+    token = System.getenv("MODRINTH_TOKEN")
+
+    projectId = rootProject.name
+
+    versionName = "${rootProject.version}"
+    versionNumber = "${rootProject.version}"
+    versionType = if (isSnapshot) "beta" else "release"
+
+    changelog = content
+
+    gameVersions.addAll(listOf(libs.versions.minecraft.get()))
+
+    uploadFile = tasks.jar.get().archiveFile.get()
+
+    loaders.addAll(listOf("paper", "folia", "purpur"))
+
+    syncBodyFrom = rootProject.file("description.md").readText(Charsets.UTF_8)
+
+    autoAddDependsOn = false
+    detectLoaders = false
+}
