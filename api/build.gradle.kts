94d2e81126bda4fbe2315cd3e42cc6f6308852e3
Ryder Belserion <contact@ryderbelserion.com>
2025-10-03 02:19:37 -0400

    [ci-skip] Update api module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,33 +1,29 @@
 plugins {
     alias(libs.plugins.fix.javadoc)
 
-    `config-java`
+    `config-paper`
 }
 
 project.group = "us.crazycrew.crazycrates"
 project.description = "The official API for CrazyCrates!"
 
-val projectVersion = "0.8"
-
-dependencies {
-    compileOnly(libs.bundles.adventure)
-}
+val projectVersion = "0.8.0"
 
 tasks {
     javadoc {
         val name = rootProject.name.replaceFirstChar { it.uppercase() }
         val options = options as StandardJavadocDocletOptions
 
-        options.encoding = Charsets.UTF_8.name()
-        options.overview("src/main/javadoc/overview.html")
-        options.use()
-        options.isDocFilesSubDirs = true
+        options.header = """<img src="https://raw.githubusercontent.com/Crazy-Crew/Branding/refs/heads/main/crazycrates/png/64x64.png" style="height:100%">"""
         options.windowTitle("$name $projectVersion API Documentation")
         options.docTitle("<h1>$name $projectVersion API</h1>")
-        options.header = """<img src="https://raw.githubusercontent.com/Crazy-Crew/Branding/refs/heads/main/crazycrates/png/64x64.png" style="height:100%">"""
-        options.bottom("Copyright © 2025 CrazyCrew")
-        options.linkSource(true)
+        options.overview("src/main/javadoc/overview.html")
         options.addBooleanOption("html5", true)
+        options.bottom("Copyright © 2025 CrazyCrew")
+        options.encoding = Charsets.UTF_8.name()
+        options.linkSource(true)
+        options.isDocFilesSubDirs = true
+        options.use()
     }
 
     withType<com.jeff_media.fixjavadoc.FixJavadoc> {
@@ -39,9 +35,18 @@ tasks {
 }
 
 publishing {
+    repositories {
+        maven {
+            url = uri("https://repo.crazycrew.us/releases/")
+
+            credentials(PasswordCredentials::class)
+            authentication.create<BasicAuthentication>("basic")
+        }
+    }
+
     publications {
         create<MavenPublication>("mavenJava") {
-            groupId = "${project.group}"
+            groupId = "${project.group}" // us.crazycrew.crazycrates
             artifactId = "api"
             version = projectVersion
 
