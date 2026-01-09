c67bb1e02d7c17feb67e1f265fc92b366d8c00da
Ryder Belserion <contact@ryderbelserion.com>
2026-01-09 13:39:25 -0500

    Add method to compare 2 ItemStacks to see if they are matching keys.

--- api/src/main/java/us/crazycrew/crazycrates/api/KeyManager.java
+++ api/src/main/java/us/crazycrew/crazycrates/api/KeyManager.java
@@ -6,6 +6,8 @@ public abstract class KeyManager<I> {
 
     public abstract boolean isKey(@NotNull final I item);
 
+    public abstract boolean isMatchingKey(@NotNull final I item, @NotNull final I comparing);
+
     public abstract String getKey(@NotNull final I item);
 
 }
--- paper/src/main/java/com/badbones69/crazycrates/paper/managers/BukkitKeyManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/paper/managers/BukkitKeyManager.java
@@ -8,6 +8,19 @@ import org.jspecify.annotations.NonNull;
 public class BukkitKeyManager extends us.crazycrew.crazycrates.api.KeyManager<ItemStack> {
 
     @Override
+    public boolean isMatchingKey(@NonNull final ItemStack item, @NonNull final ItemStack comparing) {
+        final String first_key = getKey(item);
+
+        if (first_key.isBlank()) return false;
+
+        final String second_key = getKey(comparing);
+
+        if (second_key.isBlank()) return false;
+
+        return first_key.equals(second_key);
+    }
+
+    @Override
     public final boolean isKey(@NonNull final ItemStack item) {
         return item.getPersistentDataContainer().has(ItemKeys.crate_key.getNamespacedKey());
     }
