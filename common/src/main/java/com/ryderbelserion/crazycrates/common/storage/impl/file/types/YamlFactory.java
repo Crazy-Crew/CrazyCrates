4c5c2944e4c9513ebcf85523b4170374ce139811
Ryder Belserion <contact@ryderbelserion.com>
2026-07-16 18:27:49 -0400

    Don't add missing section since the way we create entries now is easier

--- common/src/main/java/com/ryderbelserion/crazycrates/common/storage/impl/file/types/YamlFactory.java
+++ common/src/main/java/com/ryderbelserion/crazycrates/common/storage/impl/file/types/YamlFactory.java
@@ -2,9 +2,6 @@ package com.ryderbelserion.crazycrates.common.storage.impl.file.types;
 
 import com.ryderbelserion.crazycrates.common.storage.impl.file.FlatFactory;
 import org.jspecify.annotations.NullMarked;
-import org.spongepowered.configurate.CommentedConfigurationNode;
-import us.crazycrew.crazycrates.api.enums.Files;
-import java.util.List;
 
 @NullMarked
 public class YamlFactory extends FlatFactory {
@@ -20,17 +17,5 @@ public class YamlFactory extends FlatFactory {
     public void stop() {}
 
     @Override
-    public void save() {
-        final CommentedConfigurationNode configuration = Files.locations.getConfiguration();
-
-        if (!configuration.hasChild("Locations")) {
-            try {
-                configuration.node("Locations").setList(String.class, List.of());
-
-                Files.locations.save();
-            } catch (final Exception exception) {
-                exception.printStackTrace();
-            }
-        }
-    }
+    public void save() {}
 }
